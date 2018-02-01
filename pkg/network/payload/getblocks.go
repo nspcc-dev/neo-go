63072ebe75cd2ebb1c58d0f23a393bc3cb787b7b
anthdm <anthony@academiclabs.co>
2018-02-01 10:56:33 +0100

    Added getheaders payload + abstracted the fields.

--- pkg/network/payload/getblocks.go
+++ pkg/network/payload/getblocks.go
@@ -7,24 +7,17 @@ import (
 	. "github.com/anthdm/neo-go/pkg/util"
 )
 
-// GetBlocks payload
-type GetBlocks struct {
+// HashStartStop contains fields and methods to be shared with the
+// "GetBlocks" and "GetHeaders" payload.
+type HashStartStop struct {
 	// hash of latest block that node requests
 	HashStart []Uint256
 	// hash of last block that node requests
 	HashStop Uint256
 }
 
-// NewGetBlocks return a pointer to a GetBlocks object.
-func NewGetBlocks(start []Uint256, stop Uint256) *GetBlocks {
-	return &GetBlocks{
-		HashStart: start,
-		HashStop:  stop,
-	}
-}
-
 // DecodeBinary implements the payload interface.
-func (p *GetBlocks) DecodeBinary(r io.Reader) error {
+func (p *HashStartStop) DecodeBinary(r io.Reader) error {
 	var lenStart uint8
 
 	err := binary.Read(r, binary.LittleEndian, &lenStart)
@@ -36,7 +29,7 @@ func (p *GetBlocks) DecodeBinary(r io.Reader) error {
 }
 
 // EncodeBinary implements the payload interface.
-func (p *GetBlocks) EncodeBinary(w io.Writer) error {
+func (p *HashStartStop) EncodeBinary(w io.Writer) error {
 	err := binary.Write(w, binary.LittleEndian, uint8(len(p.HashStart)))
 	err = binary.Write(w, binary.LittleEndian, p.HashStart)
 	err = binary.Write(w, binary.LittleEndian, p.HashStop)
@@ -45,4 +38,17 @@ func (p *GetBlocks) EncodeBinary(w io.Writer) error {
 }
 
 // Size implements the payload interface.
-func (p *GetBlocks) Size() uint32 { return 0 }
+func (p *HashStartStop) Size() uint32 { return 0 }
+
+// GetBlocks payload
+type GetBlocks struct {
+	HashStartStop
+}
+
+// NewGetBlocks return a pointer to a GetBlocks object.
+func NewGetBlocks(start []Uint256, stop Uint256) *GetBlocks {
+	p := &GetBlocks{}
+	p.HashStart = start
+	p.HashStop = stop
+	return p
+}
