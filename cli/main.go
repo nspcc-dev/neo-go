248c36b6d5128146ad8b24bdc816591194f894a9
Steven Jack <stevenmajack@gmail.com>
2018-03-01 20:50:46 +0000

    Remove typo

--- cli/main.go
+++ cli/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	ctl := cli.NewApp()
 	ctl.Name = "neo-go"
-	ctl.Usage = "zOfficial Go client for Neo"
+	ctl.Usage = "Official Go client for Neo"
 
 	ctl.Commands = []cli.Command{
 		server.NewCommand(),
