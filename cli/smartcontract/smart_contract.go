b2a5e34aac61907bc7aa8b6b6be7f08326e73ff1
Anthony De Meulemeester <anthony@academiclabs.co>
2018-03-05 09:53:09 +0100

    RPC client (#42)
    * Started RPC package to allow querying balances and sending raw transactions for sc's
    
    * integrate invoke cmd in cli
    
    * bumped version
    
    * added sendrawtransaction to the rpc client.

--- cli/smartcontract/smart_contract.go
+++ cli/smartcontract/smart_contract.go
@@ -1,12 +1,19 @@
 package smartcontract
 
 import (
+	"context"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/CityOfZion/neo-go/pkg/rpc"
 	"github.com/CityOfZion/neo-go/pkg/vm/compiler"
 	"github.com/urfave/cli"
 )
 
 const (
-	ErrNoInput = "Input file is mandatory and should be passed using -i flag."
+	errNoInput = "Input file is mandatory and should be passed using -i flag."
 )
 
 // NewCommand returns a new contract command.
@@ -31,6 +38,17 @@ func NewCommand() cli.Command {
 				},
 			},
 			{
+				Name:   "invoke",
+				Usage:  "Test an invocation of a smart contract on the blockchain",
+				Action: testInvoke,
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:  "in, i",
+						Usage: "Input location of the avm file that needs to be invoked",
+					},
+				},
+			},
+			{
 				Name:   "opdump",
 				Usage:  "dump the opcode of a .go file",
 				Action: contractDumpOpcode,
@@ -48,7 +66,7 @@ func NewCommand() cli.Command {
 func contractCompile(ctx *cli.Context) error {
 	src := ctx.String("in")
 	if len(src) == 0 {
-		return cli.NewExitError(ErrNoInput, 1)
+		return cli.NewExitError(errNoInput, 1)
 	}
 
 	o := &compiler.Options{
@@ -62,10 +80,46 @@ func contractCompile(ctx *cli.Context) error {
 	return nil
 }
 
+func testInvoke(ctx *cli.Context) error {
+	src := ctx.String("in")
+	if len(src) == 0 {
+		return cli.NewExitError(errNoInput, 1)
+	}
+	b, err := ioutil.ReadFile(src)
+	if err != nil {
+		return cli.NewExitError(err, 1)
+	}
+
+	// For now we will hardcode the endpoint.
+	// On the long term the internal VM will run the script.
+	// TODO: remove RPC dependency, hardcoded node.
+	endpoint := "http://seed5.bridgeprotocol.io:10332"
+	opts := rpc.ClientOptions{}
+	client, err := rpc.NewClient(context.TODO(), endpoint, opts)
+	if err != nil {
+		return cli.NewExitError(err, 1)
+	}
+
+	scriptHex := hex.EncodeToString(b)
+	resp, err := client.InvokeScript(scriptHex)
+	if err != nil {
+		return cli.NewExitError(err, 1)
+	}
+
+	b, err = json.MarshalIndent(resp.Result, "", "  ")
+	if err != nil {
+		return cli.NewExitError(err, 1)
+	}
+
+	fmt.Println(string(b))
+
+	return nil
+}
+
 func contractDumpOpcode(ctx *cli.Context) error {
 	src := ctx.String("in")
 	if len(src) == 0 {
-		return cli.NewExitError(ErrNoInput, 1)
+		return cli.NewExitError(errNoInput, 1)
 	}
 	if err := compiler.DumpOpcode(src); err != nil {
 		return cli.NewExitError(err, 1)
---
+++ pkg/rpc/rpc.go
@@ -0,0 +1,57 @@
+package rpc
+
+// GetBlock returns a block by its hash or index/height. If verbose is true
+// the response will contain a pretty Block object instead of the raw hex string.
+func (c *Client) GetBlock(indexOrHash interface{}, verbose bool) (*response, error) {
+	var (
+		params = newParams(indexOrHash)
+		resp   = &response{}
+	)
+	if verbose {
+		params = newParams(indexOrHash, 1)
+	}
+	if err := c.performRequest("getblock", params, resp); err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+// GetAccountState will return detailed information about a NEO account.
+func (c *Client) GetAccountState(address string) (*AccountStateResponse, error) {
+	var (
+		params = newParams(address)
+		resp   = &AccountStateResponse{}
+	)
+	if err := c.performRequest("getaccountstate", params, resp); err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+// InvokeScipt returns the result of the given script after running it true the VM.
+// NOTE: This is a test invoke and will not affect the blokchain.
+func (c *Client) InvokeScript(script string) (*InvokeScriptResponse, error) {
+	var (
+		params = newParams(script)
+		resp   = &InvokeScriptResponse{}
+	)
+	if err := c.performRequest("invokescript", params, resp); err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+// SendRawTransaction broadcasts a transaction over the NEO network.
+// The given hex string needs to be signed with a keypair.
+// When the result of the response object is true, the TX has successfully
+// been broadcasted to the network.
+func (c *Client) SendRawTransaction(rawTX string) (*response, error) {
+	var (
+		params = newParams(rawTX)
+		resp   = &response{}
+	)
+	if err := c.performRequest("sendrawtransaction", params, resp); err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
