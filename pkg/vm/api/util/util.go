b9d20a462cf9e13523bfaaeecc84fd7a7b922cb3
Anthony De Meulemeester <anthony@academiclabs.co>
2018-04-06 15:47:53 +0200

    moved VM API packages to vm -> API

--- pkg/vm/api/util/util.go
+++ pkg/vm/api/util/util.go
@@ -1,4 +1,8 @@
 package util
 
-// Print is a VM helper signature function to print/log data.
+// Package util provides utility functions that can be used in smart contracts.
+// These functions are just signatures and provide not internal logic.
+// Only the compiler knows how to convert them to bytecode.
+
+// Print is a VM helper function to print/log data.
 func Print(v interface{}) {}
