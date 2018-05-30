5bf3caec19293084476480e70e91eaa2a51b7550
BlockChainDev <kevtheappdev@gmail.com>
2018-05-30 19:48:07 +0100

    Fixed Bug With Array Reverse Function.

--- pkg/util/array.go
+++ pkg/util/array.go
@@ -6,8 +6,9 @@ func ArrayReverse(b []byte) []byte {
 	if len(b) < 2 {
 		return b
 	}
+
 	dest := make([]byte, len(b))
-	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
+	for i, j := 0, len(b)-1; i < j+1; i, j = i+1, j-1 {
 		dest[i], dest[j] = b[j], b[i]
 	}
 	return dest
