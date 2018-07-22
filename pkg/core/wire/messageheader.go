0e1ca4c7d101ca34f5db21b1a8cae4f4ef9e45b6
BlockChainDev <kevtheappdev@gmail.com>
2018-07-22 20:48:19 +0100

    Implemented DecodeMessageHeader

--- pkg/core/wire/messageheader.go
+++ pkg/core/wire/messageheader.go
@@ -1,6 +1,7 @@
 package wire
 
 import (
+	"fmt"
 	"io"
 )
 
@@ -14,6 +15,28 @@ type MessageHeader struct {
 // Note, That there is no EncodeMessageHeader
 // As the header is implicitly inferred from
 // the message on encode
-func (h *MessageHeader) DecodeMessageHeader(r io.Reader) error {
-	return nil
+func (h *MessageHeader) DecodeMessageHeader(r io.Reader) (io.Reader, error) {
+	br := &binReader{r: r}
+	br.Read(&h.Magic)
+	fmt.Println(h.Magic)
+
+	var command [12]byte
+	br.Read(&command)
+	h.Command = CommandType(cmdByteArrayToString(command))
+	fmt.Println(h.Command)
+	br.Read(&h.Length)
+	fmt.Println(h.Length)
+	br.Read(&h.Checksum)
+	fmt.Println(h.Checksum)
+	return br.r, br.err
+}
+
+func cmdByteArrayToString(cmd [cmdSize]byte) string {
+	buf := []byte{}
+	for i := 0; i < cmdSize; i++ {
+		if cmd[i] != 0 {
+			buf = append(buf, cmd[i])
+		}
+	}
+	return string(buf)
 }
