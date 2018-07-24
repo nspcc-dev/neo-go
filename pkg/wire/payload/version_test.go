884b3ae5922ccd6e89c8706f20964e61cc3aba1b
BlockChainDev <kevtheappdev@gmail.com>
2018-07-24 18:26:43 +0100

    fixed version Message test

--- pkg/wire/payload/version_test.go
+++ pkg/wire/payload/version_test.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"testing"
 
+	"github.com/CityOfZion/neo-go/pkg/wire/protocol"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,21 +14,21 @@ func TestValidNewVersionMessage(t *testing.T) {
 	expectedIP := "127.0.0.1"
 	expectedPort := 8333
 	tcpAddrMe := &net.TCPAddr{IP: net.ParseIP(expectedIP), Port: expectedPort}
-	message, err := NewVersionMessage(tcpAddrMe, 0, true, defaultVersion)
+	message, err := NewVersionMessage(tcpAddrMe, 0, true, protocol.DefaultVersion)
 	if err != nil {
 		assert.Fail(t, err.Error())
 	}
 
 	assert.Equal(t, expectedIP, message.IP.String())
 	assert.Equal(t, uint16(expectedPort), message.Port)
-	assert.Equal(t, defaultVersion, message.Version)
+	assert.Equal(t, protocol.DefaultVersion, message.Version)
 }
 func TestEncode(t *testing.T) {
 
 	expectedIP := "127.0.0.1"
 	expectedPort := 8333
 	tcpAddrMe := &net.TCPAddr{IP: net.ParseIP(expectedIP), Port: expectedPort}
-	message, err := NewVersionMessage(tcpAddrMe, 0, true, defaultVersion)
+	message, err := NewVersionMessage(tcpAddrMe, 0, true, protocol.DefaultVersion)
 	if err != nil {
 		assert.Fail(t, err.Error())
 	}
@@ -42,7 +43,7 @@ func TestLenIsCorrect(t *testing.T) {
 	expectedIP := "127.0.0.1"
 	expectedPort := 8333
 	tcpAddrMe := &net.TCPAddr{IP: net.ParseIP(expectedIP), Port: expectedPort}
-	message, err := NewVersionMessage(tcpAddrMe, 0, true, defaultVersion)
+	message, err := NewVersionMessage(tcpAddrMe, 0, true, protocol.DefaultVersion)
 	if err != nil {
 		assert.Fail(t, err.Error())
 	}
