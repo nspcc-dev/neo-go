bac93daf42b56e1563000f7d135750581549082d
BlockChainDev <kevtheappdev@gmail.com>
2018-07-24 13:25:30 +0100

    Modified InvMessage To be compatible with c#

--- pkg/core/wire/messageinventory.go
+++ pkg/core/wire/messageinventory.go
@@ -8,7 +8,14 @@ import (
 	"github.com/CityOfZion/neo-go/pkg/core/wire/util"
 )
 
-// MARK: This and invvect.go have been Encoded and Decoded according to the specs, however not compatible with the
+type InvType uint32
+
+//Inventory types
+const (
+	InvTypeTx        InvType = 0x01
+	InvTypeBlock     InvType = 0x02
+	InvTypeConsensus InvType = 0xe0
+)
 
 const (
 	maxHashes = 0x10000000
@@ -19,41 +26,65 @@ var (
 )
 
 type InvMessage struct {
-	InvList []*InvVect
+	w      *bytes.Buffer
+	Type   InvType
+	Hashes []Uint256
 }
 
-func NewInvMessage(invList []*InvVect) (*InvMessage, error) {
-	return &InvMessage{invList}, nil
+func NewInvMessage(typ InvType, Hashes []Uint256) (*InvMessage, error) {
+	inv := &InvMessage{nil, typ, Hashes}
+	if err := inv.EncodePayload(inv.w); err != nil {
+		return nil, err
+	}
+	return inv, nil
 }
 
-func (i *InvMessage) AddInvVect(v *InvVect) error {
-	if len(i.InvList)+1 > maxHashes {
+func (i *InvMessage) AddHash(h Uint256) error {
+	if len(i.Hashes)+1 > maxHashes {
 		return MaxHashError
 	}
-	i.InvList = append(i.InvList, v)
+	i.Hashes = append(i.Hashes, h)
 	return nil
 }
 
 // Implements Messager interface
 func (v *InvMessage) DecodePayload(r io.Reader) error {
+	br := &util.BinReader{R: r}
+
+	br.Read(&v.Type)
+	listLen := br.VarUint()
+	v.Hashes = make([]Uint256, listLen)
+
+	for i := 0; i < int(listLen); i++ {
+		br.Read(&v.Hashes[i])
+	}
 	return nil
 }
 
 // Implements messager interface
 func (v *InvMessage) EncodePayload(w io.Writer) error {
 	bw := &util.BinWriter{W: w}
-	// bw.Write(v.)
+
+	bw.Write(v.Type)
+
+	lenhashes := len(v.Hashes)
+	bw.VarUint(uint64(lenhashes))
+
+	for _, hash := range v.Hashes {
+		bw.Write(hash)
+	}
+
 	return bw.Err
 }
 
 // Implements messager interface
 func (v *InvMessage) PayloadLength() uint32 {
-	return 0
+	return calculatePayloadLength(v.w)
 }
 
 // Implements messager interface
 func (v *InvMessage) Checksum() uint32 {
-	return calculateCheckSum(new(bytes.Buffer))
+	return calculateCheckSum(v.w)
 }
 
 // Implements messager interface
