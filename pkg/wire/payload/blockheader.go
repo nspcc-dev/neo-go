0c3a55f603a140c7fba91b25bd5d2af8e6166abc
BlockChainDev <kevtheappdev@gmail.com>
2018-07-25 19:40:21 +0100

    Seperated script from blockheader file

--- pkg/wire/payload/blockheader.go
+++ pkg/wire/payload/blockheader.go
@@ -12,35 +12,6 @@ var (
 	ErrPadding = errors.New("format error: padding must equal 1")
 )
 
-type Script struct {
-	InvocationScript   []byte
-	VerificationScript []byte
-}
-
-func (s *Script) EncodeScript(bw *util.BinWriter) error {
-
-	bw.VarUint(uint64(len(s.InvocationScript)))
-	bw.Write(s.InvocationScript)
-
-	bw.VarUint(uint64(len(s.VerificationScript)))
-	bw.Write(s.VerificationScript)
-
-	return bw.Err
-}
-
-func (s *Script) DecodeScript(br *util.BinReader) error {
-
-	lenb := br.VarUint()
-	s.InvocationScript = make([]byte, lenb)
-	br.Read(s.InvocationScript)
-
-	lenb = br.VarUint()
-	s.VerificationScript = make([]byte, lenb)
-	br.Read(s.VerificationScript)
-
-	return br.Err
-}
-
 type BlockHeader struct {
 	// Version of the block.
 	Version uint32 `json:"version"`
