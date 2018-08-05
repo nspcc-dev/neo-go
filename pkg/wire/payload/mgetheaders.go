936e00885691a9bf88ac40dc14fa4e41021478ea
BlockChainDev <kevtheappdev@gmail.com>
2018-08-05 21:17:01 +0100

    Refactored MSG_GetHeaders

--- pkg/wire/payload/mgetheaders.go
+++ pkg/wire/payload/mgetheaders.go
@@ -10,12 +10,15 @@ import (
 
 type GetHeadersMessage struct {
 	w         *bytes.Buffer
+	cmd       command.Type
 	hashStart []util.Uint256
 	hashStop  util.Uint256
 }
 
+// Start contains the list of all headers you want to fetch
+// End contains the list of the highest header hash you would like to fetch
 func NewGetHeadersMessage(start []util.Uint256, stop util.Uint256) (*GetHeadersMessage, error) {
-	getHeaders := &GetHeadersMessage{new(bytes.Buffer), start, stop}
+	getHeaders := &GetHeadersMessage{new(bytes.Buffer), command.GetHeaders, start, stop}
 	if err := getHeaders.EncodePayload(getHeaders.w); err != nil {
 		return nil, err
 	}
@@ -23,13 +26,34 @@ func NewGetHeadersMessage(start []util.Uint256, stop util.Uint256) (*GetHeadersM
 
 }
 
+func newAbstractGetHeaders(start []util.Uint256, stop util.Uint256, cmd command.Type) (*GetHeadersMessage, error) {
+	getHeaders, err := NewGetHeadersMessage(start, stop)
+
+	if err != nil {
+		return nil, err
+	}
+	getHeaders.cmd = cmd
+	return getHeaders, nil
+}
+
 // Implements Messager interface
 func (v *GetHeadersMessage) DecodePayload(r io.Reader) error {
+
+	buf, err := util.ReaderToBuffer(r)
+	if err != nil {
+		return err
+	}
+
+	v.w = buf
+
+	r = bytes.NewReader(buf.Bytes())
+
 	br := util.BinReader{R: r}
 	lenStart := br.VarUint()
 	v.hashStart = make([]util.Uint256, lenStart)
 	br.Read(&v.hashStart)
 	br.Read(&v.hashStop)
+
 	return br.Err
 }
 
@@ -54,5 +78,5 @@ func (v *GetHeadersMessage) Checksum() uint32 {
 
 // Implements messager interface
 func (v *GetHeadersMessage) Command() command.Type {
-	return command.GetHeaders
+	return v.cmd
 }
