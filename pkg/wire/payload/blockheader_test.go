966a50cfff042023136df83ff6f55f6427117b54
BlockChainDev <kevtheappdev@gmail.com>
2018-08-05 21:16:02 +0100

    Refactored MSG_Addr

--- pkg/wire/payload/blockheader_test.go
+++ pkg/wire/payload/blockheader_test.go
@@ -3,7 +3,6 @@ package payload
 import "testing"
 
 func Test(t *testing.T) {
+	//tests for this have been included in the mheaders_test file
 
 }
-
-//tests for this have been included in the mheaders_test file
--- pkg/wire/payload/maddr.go
+++ pkg/wire/payload/maddr.go
@@ -3,49 +3,10 @@ package payload
 import (
 	"bytes"
 	"io"
-	"time"
 
 	"github.com/CityOfZion/neo-go/pkg/wire/command"
-	"github.com/CityOfZion/neo-go/pkg/wire/protocol"
 	"github.com/CityOfZion/neo-go/pkg/wire/util"
 )
-
-// Once a VersionMessage is received, we can then store it inside of AddrMessage struct
-
-type net_addr struct {
-	Timestamp uint32
-	IP        [16]byte
-	Port      uint16
-	Service   protocol.ServiceFlag
-}
-
-func NewAddrMessage(time uint32, ip [16]byte, port uint16, service protocol.ServiceFlag) (*net_addr, error) {
-	return &net_addr{time, ip, port, service}, nil
-}
-
-func NewAddrFromVersionMessage(version VersionMessage) (*net_addr, error) {
-
-	var ip [16]byte
-
-	copy(ip[:], []byte(version.IP)[:16])
-
-	return NewAddrMessage(version.Timestamp, ip, version.Port, version.Services)
-}
-
-func (n *net_addr) EncodePayload(bw *util.BinWriter) {
-
-	bw.Write(uint32(time.Now().Unix()))
-	bw.Write(protocol.NodePeerService)
-	bw.WriteBigEnd(n.IP)
-	bw.WriteBigEnd(n.Port)
-}
-func (n *net_addr) DecodePayload(br *util.BinReader) {
-
-	br.Read(&n.Timestamp)
-	br.Read(&n.Service)
-	br.ReadBigEnd(&n.IP)
-	br.ReadBigEnd(&n.Port)
-}
 
 type AddrMessage struct {
 	w        *bytes.Buffer
@@ -54,6 +15,14 @@ type AddrMessage struct {
 
 // Implements Messager interface
 func (a *AddrMessage) DecodePayload(r io.Reader) error {
+
+	buf, err := util.ReaderToBuffer(r)
+	if err != nil {
+		return err
+	}
+
+	a.w = buf
+
 	br := &util.BinReader{R: r}
 	listLen := br.VarUint()
 
