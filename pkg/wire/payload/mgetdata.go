25f4398979a61d9c56c6fa1f33bdc52f8cc9e3b8
BlockChainDev <kevtheappdev@gmail.com>
2018-08-05 21:16:43 +0100

    Refactored MSG_GetData

--- pkg/wire/payload/mgetdata.go
+++ pkg/wire/payload/mgetdata.go
@@ -1,76 +1,19 @@
 package payload
 
 import (
-	"bytes"
-	"io"
-
 	"github.com/CityOfZion/neo-go/pkg/wire/command"
-	"github.com/CityOfZion/neo-go/pkg/wire/util"
 )
 
 type GetDataMessage struct {
-	w      *bytes.Buffer
-	Type   InvType
-	Hashes []util.Uint256
+	*InvMessage
 }
 
-func NewGetDataMessage(typ InvType, Hashes []util.Uint256) (*GetDataMessage, error) {
-	getData := &GetDataMessage{nil, typ, Hashes}
-	if err := getData.EncodePayload(getData.w); err != nil {
+func NewGetDataMessage(typ InvType) (*GetDataMessage, error) {
+	getData, err := newAbstractInv(typ, command.GetData)
+	if err != nil {
 		return nil, err
 	}
-	return getData, nil
-}
-
-func (i *GetDataMessage) AddHash(h util.Uint256) error {
-	if len(i.Hashes)+1 > maxHashes {
-		return MaxHashError
-	}
-	i.Hashes = append(i.Hashes, h)
-	return nil
-}
-
-// Implements Messager interface
-func (v *GetDataMessage) DecodePayload(r io.Reader) error {
-	br := &util.BinReader{R: r}
-
-	br.Read(&v.Type)
-	listLen := br.VarUint()
-	v.Hashes = make([]util.Uint256, listLen)
-
-	for i := 0; i < int(listLen); i++ {
-		br.Read(&v.Hashes[i])
-	}
-	return nil
-}
-
-// Implements messager interface
-func (v *GetDataMessage) EncodePayload(w io.Writer) error {
-	bw := &util.BinWriter{W: w}
-
-	bw.Write(v.Type)
-
-	lenhashes := len(v.Hashes)
-	bw.VarUint(uint64(lenhashes))
-
-	for _, hash := range v.Hashes {
-		bw.Write(hash)
-	}
-
-	return bw.Err
-}
-
-// Implements messager interface
-func (v *GetDataMessage) PayloadLength() uint32 {
-	return util.CalculatePayloadLength(v.w)
-}
-
-// Implements messager interface
-func (v *GetDataMessage) Checksum() uint32 {
-	return util.CalculateCheckSum(v.w)
-}
-
-// Implements messager interface
-func (v *GetDataMessage) Command() command.Type {
-	return command.GetData
+	return &GetDataMessage{
+		getData,
+	}, nil
 }
