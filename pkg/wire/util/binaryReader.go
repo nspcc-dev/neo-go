a0cb538a9a4ad207565774d1b7aabcdead332577
BlockChainDev <kevtheappdev@gmail.com>
2018-08-06 18:19:00 +0100

    refactored binaryReader; added r.Err

--- pkg/wire/util/binaryReader.go
+++ pkg/wire/util/binaryReader.go
@@ -14,37 +14,32 @@ func (r *BinReader) Read(v interface{}) {
 	if r.Err != nil {
 		return
 	}
-	binary.Read(r.R, binary.LittleEndian, v)
+	r.Err = binary.Read(r.R, binary.LittleEndian, v)
 }
 func (r *BinReader) ReadBigEnd(v interface{}) {
 	if r.Err != nil {
 		return
 	}
-	binary.Read(r.R, binary.BigEndian, v)
+	r.Err = binary.Read(r.R, binary.BigEndian, v)
 }
-
-// func (br *BinReader) Bytes() {
-// 	by, _ := util.ReaderToBuffer(br.R)
-// 	return by
-// }
 
 func (r *BinReader) VarUint() uint64 {
 	var b uint8
-	binary.Read(r.R, binary.LittleEndian, &b)
+	r.Err = binary.Read(r.R, binary.LittleEndian, &b)
 
 	if b == 0xfd {
 		var v uint16
-		binary.Read(r.R, binary.LittleEndian, &v)
+		r.Err = binary.Read(r.R, binary.LittleEndian, &v)
 		return uint64(v)
 	}
 	if b == 0xfe {
 		var v uint32
-		binary.Read(r.R, binary.LittleEndian, &v)
+		r.Err = binary.Read(r.R, binary.LittleEndian, &v)
 		return uint64(v)
 	}
 	if b == 0xff {
 		var v uint64
-		binary.Read(r.R, binary.LittleEndian, &v)
+		r.Err = binary.Read(r.R, binary.LittleEndian, &v)
 		return v
 	}
 
