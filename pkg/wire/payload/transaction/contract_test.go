a157f814ea6c4a265e4d338de0c2c03d9bc36f05
BlockChainDev <kevtheappdev@gmail.com>
2018-08-06 18:37:36 +0100

    added test for correct hash in claim and contract

--- pkg/wire/payload/transaction/contract_test.go
+++ pkg/wire/payload/transaction/contract_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestEncodeDecode(t *testing.T) {
+func TestEncodeDecodeContract(t *testing.T) {
 
 	// mainnet transaction: bdf6cc3b9af12a7565bda80933a75ee8cef1bc771d0d58effc08e4c8b436da79
 	rawtx := "80000001888da99f8f497fd65c4325786a09511159c279af4e7eb532e9edd628c87cc1ee0000019b7cffdaa674beae0f930ebe6085af9093e5fe56b34a5c220ccdcf6efc336fc50082167010000000a8666b4830229d6a1a9b80f6088059191c122d2b0141409e79e132290c82916a88f1a3db5cf9f3248b780cfece938ab0f0812d0e188f3a489c7d1a23def86bd69d863ae67de753b2c2392e9497eadc8eb9fc43aa52c645232103e2f6a334e05002624cf616f01a62cff2844c34a3b08ca16048c259097e315078ac"
@@ -38,6 +38,7 @@ func TestEncodeDecode(t *testing.T) {
 	err = c.Encode(buf)
 	assert.Equal(t, nil, err)
 
+	assert.Equal(t, "bdf6cc3b9af12a7565bda80933a75ee8cef1bc771d0d58effc08e4c8b436da79", c.Hash.String())
 	assert.Equal(t, rawtxBytes, buf.Bytes())
 
 }
