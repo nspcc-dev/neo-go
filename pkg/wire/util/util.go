19339d998f569f0c58f56e4633893e99db7b038f
BlockChainDev <kevtheappdev@gmail.com>
2018-08-09 16:48:21 +0100

    Created checksum package and moved methods from util

--- pkg/wire/util/util.go
+++ pkg/wire/util/util.go
@@ -3,7 +3,7 @@ package util
 import (
 	"bytes"
 	"crypto/sha256"
-	"encoding/binary"
+
 	"io"
 	"io/ioutil"
 )
@@ -13,22 +13,11 @@ func CalculatePayloadLength(buf *bytes.Buffer) uint32 {
 
 	return uint32(buf.Len())
 }
-func CalculateCheckSum(buf *bytes.Buffer) uint32 {
-
-	checksum := SumSHA256(SumSHA256(buf.Bytes()))
-	return binary.LittleEndian.Uint32(checksum[:4])
-}
 
 func SumSHA256(b []byte) []byte {
 	h := sha256.New()
 	h.Write(b)
 	return h.Sum(nil)
-}
-
-func CompareChecksum(have uint32, b []byte) bool {
-	sum := SumSHA256(SumSHA256(b))[:4]
-	want := binary.LittleEndian.Uint32(sum)
-	return have == want
 }
 
 func CalculateHash(f func(bw *BinWriter)) (Uint256, error) {
