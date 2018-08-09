d0a036e214f15c643704e1337bf5e42d66679887
decentralisedkev <37423678+decentralisedkev@users.noreply.github.com>
2018-08-09 21:23:19 +0100

    Update main.go

--- cli/main.go
+++ cli/main.go
@@ -2,16 +2,9 @@ package main
 
 import (
 	"os"
-	"github.com/urfave/cli"
+	
 )
 
 func main() {
-	ctl := cli.NewApp()
-	ctl.Name = "neo-go"
-	ctl.Usage = "Official Go client for Neo"
 
-	ctl.Commands = []cli.Command{
-	}
-
-	ctl.Run(os.Args)
 }
