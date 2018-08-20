86715511d0bd9192cd2331dad7247b54c7f23953
Anthony De Meulemeester <anthony@academiclabs.co>
2018-08-20 10:59:35 +0200

    Added examples folder
    Imported from CityOfZion/neo-storm (77d0e5949470b2648acd37c71116f4469197772b).

---
+++ interop/runtime/runtime.go
@@ -0,0 +1,6 @@
+package runtime
+
+// CheckWitness verifies if the given hash is the invoker of the contract.
+func CheckWitness(hash []byte) bool {
+	return true
+}
---
+++ interop/util/util.go
@@ -0,0 +1,6 @@
+package util
+
+// FromAddress is an utility function that converts an NEO address to its hash.
+func FromAddress(address string) []byte {
+	return nil
+}
