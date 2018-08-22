8bfaed0e4b938f24c7748ecf0f7672925d9fffa8
Anthony De Meulemeester <anthony@academiclabs.co>
2018-08-22 10:12:57 +0200

    added in the crypto helper api for smart contracts. (CityOfZion/neo-storm#15)
    Imported from CityOfZion/neo-storm (ad6e9dae46846f97a7da6dd48bf55ad554d612b3).

---
+++ interop/crypto/crypto.go
@@ -0,0 +1,24 @@
+package crypto
+
+// Package crypto provides function signatures that can be used inside
+// smart contracts that are written in the neo-storm framework.
+
+// SHA1 computes the sha1 hash of b.
+func SHA1(b []byte) []byte {
+	return nil
+}
+
+// SHA256 computes the sha256 hash of b.
+func SHA256(b []byte) []byte {
+	return nil
+}
+
+// Hash160 computes the sha256 + ripemd160 of b.
+func Hash160(b []byte) []byte {
+	return nil
+}
+
+// Hash256 computes the sha256^2 hash of b.
+func Hash256(b []byte) []byte {
+	return nil
+}
