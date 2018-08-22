b3037cd598a6de6b6e2992fdc74858c31c3c1879
Jeroen Peeters <contact@jeroenpeeters.me>
2018-08-22 18:23:19 +0200

    Token Example (CityOfZion/neo-storm#12)
    * feat: add token example
    
    * feat: code splitted package
    
    * feat: use updated apis
    
    Imported from CityOfZion/neo-storm (e2bab450d7355b559ae2d70a87f557e8a6dbfff6).

---
+++ examples/token/token.go
@@ -0,0 +1,70 @@
+package token_contract
+
+import (
+	"token"
+
+	"github.com/CityOfZion/neo-storm/interop/storage"
+	"github.com/CityOfZion/neo-storm/interop/util"
+)
+
+const (
+	decimals   = 8
+	multiplier = 100000000
+)
+
+var owner = util.FromAddress("AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y")
+
+// CreateToken initializes the Token Interface for the Smart Contract to operate with
+func CreateToken() token.Token {
+	return token.Token{
+		Name:           "Awesome NEO Token",
+		Symbol:         "ANT",
+		Decimals:       decimals,
+		Owner:          owner,
+		TotalSupply:    11000000 * multiplier,
+		CirculationKey: "TokenCirculation",
+	}
+}
+
+// Main function = contract entry
+func Main(operation string, args []interface{}) interface{} {
+	token := CreateToken()
+
+	if operation == "name" {
+		return token.Name
+	}
+	if operation == "symbol" {
+		return token.Symbol
+	}
+	if operation == "decimals" {
+		return token.Decimals
+	}
+
+	// The following operations need ctx
+	ctx := storage.GetContext()
+
+	if operation == "totalSupply" {
+		return token.GetSupply(ctx)
+	}
+	if operation == "balanceOf" {
+		hodler := args[0].([]byte)
+		return token.BalanceOf(ctx, hodler)
+	}
+	if operation == "transfer" && CheckArgs(args, 3) {
+		from := args[0].([]byte)
+		to := args[1].([]byte)
+		amount := args[2].(int)
+		return token.Transfer(ctx, from, to, amount)
+	}
+
+	return true
+}
+
+// CheckArgs checks args array against a length indicator
+func CheckArgs(args []interface{}, length int) bool {
+	if len(args) == length {
+		return true
+	}
+
+	return false
+}
