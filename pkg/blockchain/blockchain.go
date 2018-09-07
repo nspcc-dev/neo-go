a2e475bf441c1959782339ca70c3686db1b85e59
BlockChainDev <kevtheappdev@gmail.com>
2018-09-07 20:07:38 +0100

    blockchain refactor, Added Validate Headers + AddHeaders

--- pkg/blockchain/blockchain.go
+++ pkg/blockchain/blockchain.go
@@ -2,11 +2,15 @@ package blockchain
 
 import (
 	"bytes"
+	"encoding/hex"
 	"errors"
 	"fmt"
 
+	"github.com/CityOfZion/neo-go/pkg/chainparams"
+
 	"github.com/CityOfZion/neo-go/pkg/database"
 	"github.com/CityOfZion/neo-go/pkg/wire/payload"
+	"github.com/CityOfZion/neo-go/pkg/wire/protocol"
 )
 
 var (
@@ -16,12 +20,62 @@ var (
 
 // Blockchain holds the state of the chain
 type Chain struct {
-	db *database.LDB
+	db  *database.LDB
+	net protocol.Magic
 }
 
-func New(db *database.LDB) *Chain {
+func New(db *database.LDB, net protocol.Magic) *Chain {
+
+	marker := []byte("HasBeenInitialisedAlready")
+
+	_, err := db.Get(marker)
+
+	if err != nil {
+		// This is a new db
+		fmt.Println("New Database initialisation")
+		db.Put(marker, []byte{})
+
+		// We add the genesis block into the db
+		// along with the indexes for it
+		if net == protocol.MainNet {
+
+			genesisBlock, err := hex.DecodeString(chainparams.GenesisBlock)
+			if err != nil {
+				fmt.Println("Could not add genesis header into db")
+				db.Delete(marker)
+				return nil
+			}
+			r := bytes.NewReader(genesisBlock)
+			b := payload.Block{}
+			err = b.Decode(r)
+
+			if err != nil {
+				fmt.Println("could not Decode genesis block")
+				db.Delete(marker)
+				return nil
+			}
+			err = db.AddHeader(&b.BlockBase)
+			if err != nil {
+				fmt.Println("Could not add genesis header")
+				db.Delete(marker)
+				return nil
+			}
+			err = db.AddTransactions(b.Hash, b.Txs)
+			if err != nil {
+				fmt.Println("Could not add Genesis Transactions")
+				db.Delete(marker)
+				return nil
+			}
+		}
+		if net == protocol.TestNet {
+			fmt.Println("TODO: Setup the genesisBlock for TestNet")
+			return nil
+		}
+
+	}
 	return &Chain{
 		db,
+		net,
 	}
 }
 func (c *Chain) AddBlock(msg *payload.BlockMessage) error {
@@ -57,4 +111,84 @@ func validateBlock(msg *payload.BlockMessage) bool {
 
 func (c *Chain) verifyBlock(msg *payload.BlockMessage) bool {
 	return true
+}
+
+// This will add a header into the db,
+// indexing it also, this method will not
+// run any checks, like if it links with a header
+// previously in the db
+// func (c *Chain) addHeaderNoCheck(header *payload.BlockBase) error {
+
+// }
+
+//addHeaders is not safe for concurrent access
+func (c *Chain) ValidateHeaders(msg *payload.HeadersMessage) error {
+	fmt.Println("Checking and Adding Headers into List")
+
+	table := database.NewTable(c.db, database.HEADER)
+
+	latestHash, err := table.Get(database.LATESTHEADER)
+	if err != nil {
+		fmt.Println("Could not find the latest hash in the database")
+		return err
+	}
+
+	key := latestHash
+	val, err := table.Get(key)
+
+	lastHeader := &payload.BlockBase{}
+	err = lastHeader.Decode(bytes.NewReader(val))
+	if err != nil {
+		fmt.Println("Error decoding blockbase", err)
+		return err
+	}
+
+	// TODO:Maybe we should sort these headers using the Index
+	// We should not get them in mixed order, but doing it would not be expensive
+	// If they are already in order
+
+	// Do checks on headers
+	for _, currentHeader := range msg.Headers {
+
+		if lastHeader == nil {
+			// This should not happen as genesis header is added if new
+			// database, however we check nonetheless
+			return errors.New("Previous Header is nil")
+		}
+
+		// Check current hash links with previous
+		if currentHeader.PrevHash != lastHeader.Hash {
+			fmt.Println("CurrentHeader says PrevHash=", currentHeader.PrevHash)
+			fmt.Println("LastHeader says Hash=", lastHeader.Hash)
+			return errors.New("Last Header hash != current header Prev hash")
+		}
+
+		// Check current Index is one more than the previous Index
+		if currentHeader.Index != lastHeader.Index+1 {
+			fmt.Println("CurrentHeader says Index=", currentHeader.Index)
+			fmt.Println("LastHeader says Index=", lastHeader.Index)
+			return errors.New("Last Header Index != current header Index")
+		}
+
+		// Check current timestamp is more than the previous header's timestamp
+		if lastHeader.Timestamp > currentHeader.Timestamp {
+			return errors.New("Timestamp of Previous Header is more than Timestamp of current Header")
+		}
+
+		// NONONO:Do not check if current is more than 15 secs in future
+		// some blocks had delay from forks in past.
+
+		// NOTE: These are the only non-contextual checks we can do without the blockchain state
+		lastHeader = currentHeader
+	}
+	return nil
+}
+
+func (c *Chain) AddHeaders(msg *payload.HeadersMessage) error {
+	for _, header := range msg.Headers {
+		if err := c.db.AddHeader(header); err != nil {
+			return err
+		}
+	}
+	return nil
 }
