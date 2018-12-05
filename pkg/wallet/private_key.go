e1bef2063e555ae1f5e5dbda64cfd1dd51daee72
anastasia prasolova <anastasia@nspcc.ru>
2018-12-05 14:01:31 +0300

    comments fix

--- pkg/wallet/private_key.go
+++ pkg/wallet/private_key.go
@@ -58,7 +58,7 @@ func NewPrivateKeyFromBytes(b []byte) (*PrivateKey, error) {
 	return &PrivateKey{b}, nil
 }
 
-// NewPrivateKeyFromRawBytes returns a NEO PrivateKey from the protobuf serialized x509 private key.
+// NewPrivateKeyFromRawBytes returns a NEO PrivateKey from the ASN.1 serialized keys.
 func NewPrivateKeyFromRawBytes(b []byte) (*PrivateKey, error) {
 	privkey, err := x509.ParseECPrivateKey(b)
 	if err != nil {
