a5e2df69422ba2f318fdf14af048065897c7f6ef
Anastasia Prasolova <a-prasolova1507@yandex.ru>
2019-02-07 14:44:51 +0300

    small fix of utxo calculation on raw transaction build (#126)
    * small fixes
    
    * gofmt
    
    * fix in raw tx build
    
    * fixes after review

--- pkg/rpc/txBuilder.go
+++ pkg/rpc/txBuilder.go
@@ -48,8 +48,10 @@ func CreateRawContractTransaction(params ContractTxParams) (*transaction.Transac
 		tx.AddInput(&input)
 	}
 
-	senderOutput = transaction.NewOutput(assetID, spent-amount, fromAddressHash)
-	tx.AddOutput(senderOutput)
+	if senderUnspent := spent - amount; senderUnspent > 0 {
+		senderOutput = transaction.NewOutput(assetID, senderUnspent, fromAddressHash)
+		tx.AddOutput(senderOutput)
+	}
 	receiverOutput = transaction.NewOutput(assetID, amount, toAddressHash)
 	tx.AddOutput(receiverOutput)
 
