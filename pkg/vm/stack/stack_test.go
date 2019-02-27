8d55ea12f00c6d3d1cea2b02e353754d61e1b2db
BlockChainDev <kevtheappdev@gmail.com>
2019-02-27 21:40:31 +0000

    VM: removed helper functions from stack_test.go

--- pkg/vm/stack/stack_test.go
+++ pkg/vm/stack/stack_test.go
@@ -159,30 +159,3 @@ func TestStackInsert(t *testing.T) {
 	assert.Equal(t, true, pop3.Equal(a))
 
 }
-
-// helper functions
-func testPeakInteger(t *testing.T, tStack *RandomAccess, n uint16) *Int {
-	stackElement, err := tStack.Peek(n)
-	assert.Nil(t, err)
-	item, err := stackElement.Integer()
-	if err != nil {
-		t.Fail()
-	}
-	return item
-}
-
-func testPopInteger(t *testing.T, tStack *RandomAccess) *Int {
-	stackElement, err := tStack.Pop()
-	assert.Nil(t, err)
-	item, err := stackElement.Integer()
-	if err != nil {
-		t.Fail()
-	}
-	return item
-}
-
-func testMakeStackInt(t *testing.T, num int64) *Int {
-	a, err := NewInt(big.NewInt(num))
-	assert.Nil(t, err)
-	return a
-}
