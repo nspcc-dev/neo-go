64491a4d83ab99f81ee10c708723e47fb3c54838
BlockChainDev <kevtheappdev@gmail.com>
2019-02-27 21:38:11 +0000

    VM: Add Sub, Mul, Mod LSH, RSH

--- pkg/vm/stack/Int.go
+++ pkg/vm/stack/Int.go
@@ -28,7 +28,42 @@ func (i *Int) Equal(s *Int) bool {
 // Add will add two stackIntegers together
 func (i *Int) Add(s *Int) (*Int, error) {
 	return &Int{
+		val: new(big.Int).Add(i.val, s.val),
+	}, nil
+}
+
+// Sub will subtract two stackIntegers together
+func (i *Int) Sub(s *Int) (*Int, error) {
+	return &Int{
 		val: new(big.Int).Sub(i.val, s.val),
+	}, nil
+}
+
+// Mul will multiply two stackIntegers together
+func (i *Int) Mul(s *Int) (*Int, error) {
+	return &Int{
+		val: new(big.Int).Mul(i.val, s.val),
+	}, nil
+}
+
+// Mod will take the mod of two stackIntegers together
+func (i *Int) Mod(s *Int) (*Int, error) {
+	return &Int{
+		val: new(big.Int).Mod(i.val, s.val),
+	}, nil
+}
+
+// Rsh will shift the integer b to the right by `n` bits
+func (i *Int) Rsh(n *Int) (*Int, error) {
+	return &Int{
+		val: new(big.Int).Rsh(i.val, uint(n.val.Int64())),
+	}, nil
+}
+
+// Lsh will shift the integer b to the left by `n` bits
+func (i *Int) Lsh(n *Int) (*Int, error) {
+	return &Int{
+		val: new(big.Int).Lsh(i.val, uint(n.val.Int64())),
 	}, nil
 }
 
---
+++ pkg/vm/stack/int_test.go
@@ -0,0 +1,57 @@
+package stack
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAdd(t *testing.T) {
+	a := testMakeStackInt(t, 10)
+	b := testMakeStackInt(t, 20)
+	expected := testMakeStackInt(t, 30)
+	c, err := a.Add(b)
+	assert.Nil(t, err)
+	assert.Equal(t, true, expected.Equal(c))
+}
+func TestSub(t *testing.T) {
+	a := testMakeStackInt(t, 30)
+	b := testMakeStackInt(t, 200)
+	expected := testMakeStackInt(t, 170)
+	c, err := b.Sub(a)
+	assert.Nil(t, err)
+	assert.Equal(t, true, expected.Equal(c))
+}
+func TestMul(t *testing.T) {
+	a := testMakeStackInt(t, 10)
+	b := testMakeStackInt(t, 20)
+	expected := testMakeStackInt(t, 200)
+	c, err := a.Mul(b)
+	assert.Nil(t, err)
+	assert.Equal(t, true, expected.Equal(c))
+}
+func TestMod(t *testing.T) {
+	a := testMakeStackInt(t, 10)
+	b := testMakeStackInt(t, 20)
+	expected := testMakeStackInt(t, 10)
+	c, err := a.Mod(b)
+	assert.Nil(t, err)
+	assert.Equal(t, true, expected.Equal(c))
+}
+func TestLsh(t *testing.T) {
+	a := testMakeStackInt(t, 23)
+	b := testMakeStackInt(t, 8)
+	expected := testMakeStackInt(t, 5888)
+	c, err := a.Lsh(b)
+	assert.Nil(t, err)
+	assert.Equal(t, true, expected.Equal(c))
+}
+
+func TestRsh(t *testing.T) {
+	a := testMakeStackInt(t, 128)
+	b := testMakeStackInt(t, 3)
+	expected := testMakeStackInt(t, 16)
+	c, err := a.Rsh(b)
+	assert.Nil(t, err)
+	assert.Equal(t, true, expected.Equal(c))
+}
