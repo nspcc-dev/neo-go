c3d03f2b4a3951d7713473ffa1efdf9e18aacccf
BlockChainDev <kevtheappdev@gmail.com>
2019-03-21 22:50:59 +0000

    [ConnMgr]
    Add comment for Request

--- pkg/connmgr/request.go
+++ pkg/connmgr/request.go
@@ -4,6 +4,8 @@ import (
 	"net"
 )
 
+// Request is a layer on top of connection and allows us to add metadata to the net.Conn
+// that the connection manager can use to determine whether to retry and other useful heuristics
 type Request struct {
 	Conn      net.Conn
 	Addr      string
