79d3a3ec5e587bb9b508281005d2217f8fa3ff05
DauTT <dautruongtan@gmail.com>
2019-03-26 23:23:59 +0100

    Implemented INC, DEC opcode

--- pkg/vm/vm_ops_maths_test.go
+++ pkg/vm/vm_ops_maths_test.go
@@ -8,6 +8,56 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestIncOp(t *testing.T) {
+
+	v := VM{}
+
+	a, err := stack.NewInt(big.NewInt(20))
+	if err != nil {
+		t.Fail()
+	}
+
+	ctx := stack.NewContext([]byte{})
+	ctx.Estack.Push(a)
+
+	v.executeOp(stack.INC, ctx)
+
+	// Stack should have one item
+	assert.Equal(t, 1, ctx.Estack.Len())
+
+	item, err := ctx.Estack.PopInt()
+	if err != nil {
+		t.Fail()
+	}
+
+	assert.Equal(t, int64(21), item.Value().Int64())
+}
+
+func TestDecOp(t *testing.T) {
+
+	v := VM{}
+
+	a, err := stack.NewInt(big.NewInt(20))
+	if err != nil {
+		t.Fail()
+	}
+
+	ctx := stack.NewContext([]byte{})
+	ctx.Estack.Push(a)
+
+	v.executeOp(stack.DEC, ctx)
+
+	// Stack should have one item
+	assert.Equal(t, 1, ctx.Estack.Len())
+
+	item, err := ctx.Estack.PopInt()
+	if err != nil {
+		t.Fail()
+	}
+
+	assert.Equal(t, int64(19), item.Value().Int64())
+}
+
 func TestAddOp(t *testing.T) {
 
 	v := VM{}
