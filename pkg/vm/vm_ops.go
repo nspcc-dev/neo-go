14e0ab48dd471c2bf8e4465ab90de0524ac5161c
dauTT <30392990+dauTT@users.noreply.github.com>
2019-03-28 19:30:36 +0100

    VM: Implemented SIGN, NEGATE opcode (#232)
    * Implemented SIGN, NEGATE opcode

--- pkg/vm/vm_ops.go
+++ pkg/vm/vm_ops.go
@@ -7,6 +7,8 @@ type stackInfo func(op stack.Instruction, ctx *stack.Context, istack *stack.Invo
 var opFunc = map[stack.Instruction]stackInfo{
 	stack.INC:         Inc,
 	stack.DEC:         Dec,
+	stack.SIGN:        Sign,
+	stack.NEGATE:      Negate,
 	stack.ADD:         Add,
 	stack.SUB:         Sub,
 	stack.PUSHBYTES1:  PushNBytes,
