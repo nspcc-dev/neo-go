nspcc-dev/neo-go

commit 03ff2976ed6b6fa0a77646dcedbe5dd71c587860

io: refactoring for using WriteVarBytes instead of WriteLE
goal is to be consistent with C# implementation.
For writing []byte WriteBytes used and for byte - WriteVarByte.

cli/server/server.go
@@ -177,8 +177,7 @@ func dumpDB(ctx *cli.Context) error {
 		buf := io.NewBufBinWriter()
 		b.EncodeBinary(buf.BinWriter)
 		bytes := buf.Bytes()
-		writer.WriteLE(uint32(len(bytes)))
-		writer.WriteLE(bytes)
+		writer.WriteVarBytes(bytes)
 		if writer.Err != nil {
 			return cli.NewExitError(err, 1)
 		}

pkg/consensus/payload.go
@@ -169,7 +169,7 @@ func (p Payload) EncodeBinaryUnsigned(w *io.BinWriter) {
 
 	ww := io.NewBufBinWriter()
 	p.message.EncodeBinary(ww.BinWriter)
-	w.WriteBytes(ww.Bytes())
+	w.WriteVarBytes(ww.Bytes())
 }
 
 // EncodeBinary implements io.Serializable interface.

pkg/consensus/recovery_message.go
@@ -100,7 +100,7 @@ func (p *changeViewCompact) EncodeBinary(w *io.BinWriter) {
 	w.WriteLE(p.ValidatorIndex)
 	w.WriteLE(p.OriginalViewNumber)
 	w.WriteLE(p.Timestamp)
-	w.WriteBytes(p.InvocationScript)
+	w.WriteVarBytes(p.InvocationScript)
 }
 
 // DecodeBinary implements io.Serializable interface.
@@ -116,7 +116,7 @@ func (p *commitCompact) EncodeBinary(w *io.BinWriter) {
 	w.WriteLE(p.ViewNumber)
 	w.WriteLE(p.ValidatorIndex)
 	w.WriteBE(p.Signature)
-	w.WriteBytes(p.InvocationScript)
+	w.WriteVarBytes(p.InvocationScript)
 }
 
 // DecodeBinary implements io.Serializable interface.
@@ -128,7 +128,7 @@ func (p *preparationCompact) DecodeBinary(r *io.BinReader) {
 // EncodeBinary implements io.Serializable interface.
 func (p *preparationCompact) EncodeBinary(w *io.BinWriter) {
 	w.WriteLE(p.ValidatorIndex)
-	w.WriteBytes(p.InvocationScript)
+	w.WriteVarBytes(p.InvocationScript)
 }
 
 // AddPayload implements payload.RecoveryMessage interface.

pkg/core/blockchain.go
@@ -1508,7 +1508,7 @@ func (bc *Blockchain) verifyBlockWitnesses(block *Block, prevHeader *Header) err
 
 func hashAndIndexToBytes(h util.Uint256, index uint32) []byte {
 	buf := io.NewBufBinWriter()
-	buf.WriteLE(h.BytesReverse())
+	buf.WriteBytes(h.BytesReverse())
 	buf.WriteLE(index)
 	return buf.Bytes()
 }

pkg/core/blockchain_state.go
@@ -69,7 +69,7 @@ func (state *BlockChainState) storeAsBlock(block *Block, sysFee uint32) error {
 	if err != nil {
 		return err
 	}
-	buf.WriteLE(b)
+	buf.WriteBytes(b)
 	if buf.Err != nil {
 		return buf.Err
 	}
@@ -79,7 +79,7 @@ func (state *BlockChainState) storeAsBlock(block *Block, sysFee uint32) error {
 // storeAsCurrentBlock stores the given block witch prefix SYSCurrentBlock.
 func (state *BlockChainState) storeAsCurrentBlock(block *Block) error {
 	buf := io.NewBufBinWriter()
-	buf.WriteLE(block.Hash().BytesReverse())
+	buf.WriteBytes(block.Hash().BytesReverse())
 	buf.WriteLE(block.Index)
 	return state.store.Put(storage.SYSCurrentBlock.Bytes(), buf.Bytes())
 }

pkg/core/contract_state.go
@@ -52,7 +52,7 @@ func (cs *ContractState) DecodeBinary(br *io.BinReader) {
 
 // EncodeBinary implements Serializable interface.
 func (cs *ContractState) EncodeBinary(bw *io.BinWriter) {
-	bw.WriteBytes(cs.Script)
+	bw.WriteVarBytes(cs.Script)
 	bw.WriteArray(cs.ParamList)
 	bw.WriteLE(cs.ReturnType)
 	bw.WriteLE(cs.Properties)

pkg/core/storage_item.go
@@ -53,7 +53,7 @@ func deleteStorageItemInStore(s storage.Store, scripthash util.Uint160, key []by
 
 // EncodeBinary implements Serializable interface.
 func (si *StorageItem) EncodeBinary(w *io.BinWriter) {
-	w.WriteBytes(si.Value)
+	w.WriteVarBytes(si.Value)
 	w.WriteLE(si.IsConst)
 }
 

pkg/core/transaction/attribute.go
@@ -55,18 +55,18 @@ func (attr *Attribute) EncodeBinary(bw *io.BinWriter) {
 	bw.WriteLE(&attr.Usage)
 	switch attr.Usage {
 	case ECDH02, ECDH03:
-		bw.WriteLE(attr.Data[1:])
+		bw.WriteBytes(attr.Data[1:])
 	case Description, Remark, Remark1, Remark2, Remark3, Remark4,
 		Remark5, Remark6, Remark7, Remark8, Remark9, Remark10, Remark11,
 		Remark12, Remark13, Remark14, Remark15:
-		bw.WriteBytes(attr.Data)
+		bw.WriteVarBytes(attr.Data)
 	case DescriptionURL:
 		var urllen = uint8(len(attr.Data))
 		bw.WriteLE(urllen)
 		fallthrough
 	case Script, ContractHash, Vote, Hash1, Hash2, Hash3, Hash4, Hash5, Hash6,
 		Hash7, Hash8, Hash9, Hash10, Hash11, Hash12, Hash13, Hash14, Hash15:
-		bw.WriteLE(attr.Data)
+		bw.WriteBytes(attr.Data)
 	default:
 		bw.Err = fmt.Errorf("failed encoding TX attribute usage: 0x%2x", attr.Usage)
 	}

commit 7aa7490fb256d0469d7ed6d836ae33d6f1b68800

cli: fix wrong db dump format
It was broken by 03ff2976ed6b6fa0a77646dcedbe5dd71c587860 that changed fixed
32-bit length encoding to VarBytes.

cli/server/server.go
@@ -193,7 +193,8 @@ func dumpDB(ctx *cli.Context) error {
 		buf := io.NewBufBinWriter()
 		b.EncodeBinary(buf.BinWriter)
 		bytes := buf.Bytes()
-		writer.WriteVarBytes(bytes)
+		writer.WriteU32LE(uint32(len(bytes)))
+		writer.WriteBytes(bytes)
 		if writer.Err != nil {
 			return cli.NewExitError(err, 1)
 		}