2316e2c4a7cb65559679a5fcf58138e12994ce8c
Roman Khimov <roman@nspcc.ru>
2020-03-28 13:59:42 +0300

    rpc: fix stack field name for invocation results
    Follow C# implementation:
    https://docs.neo.org/docs/en-us/reference/rpc/latest-version/api/invokefunction.html

--- pkg/rpc/response/result/invoke.go
+++ pkg/rpc/response/result/invoke.go
@@ -7,8 +7,8 @@ import (
 // Invoke represents code invocation result and is used by several RPC calls
 // that invoke functions, scripts and generic bytecode.
 type Invoke struct {
-	State       string `json:"state"`
-	GasConsumed string `json:"gas_consumed"`
-	Script      string `json:"script"`
-	Stack       []smartcontract.Parameter
+	State       string                    `json:"state"`
+	GasConsumed string                    `json:"gas_consumed"`
+	Script      string                    `json:"script"`
+	Stack       []smartcontract.Parameter `json:"stack"`
 }
