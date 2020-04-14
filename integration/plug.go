9630f6322465892246cbec9aca3c976da4f083d2
Evgenii Stratonikov <evgeniy@nspcc.ru>
2020-04-14 12:32:31 +0300

    .circleci: enable -coverpkg=all
    We want to count test for coverage irregardless
    of where it is located. Previously error "no non-test Go files"
    was preventing us from doing so. This commit fixes it.

---
+++ integration/plug.go
@@ -0,0 +1,6 @@
+// Package integration contains integration tests.
+// See README.md for more info.
+// This file exists with the sole purpose to get rid of
+// "go build github.com/nspcc-dev/neo-go/integration: no non-test Go files in *"
+// when running `go test -coverpkg=all`.
+package integration
