5ab4b9284bb01d6decdfbf46f00768813378179e
Evgenii Stratonikov <evgeniy@nspcc.ru>
2020-05-06 16:00:16 +0300

    core: remove unused function from tests

--- pkg/core/helper_test.go
+++ pkg/core/helper_test.go
@@ -14,7 +14,6 @@ import (
 	"github.com/nspcc-dev/neo-go/pkg/core/storage"
 	"github.com/nspcc-dev/neo-go/pkg/core/transaction"
 	"github.com/nspcc-dev/neo-go/pkg/crypto/hash"
-	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
 	"github.com/nspcc-dev/neo-go/pkg/internal/testchain"
 	"github.com/nspcc-dev/neo-go/pkg/internal/testserdes"
 	"github.com/nspcc-dev/neo-go/pkg/io"
@@ -139,13 +138,6 @@ func newDumbBlock() *block.Block {
 			{Type: transaction.IssueType},
 		},
 	}
-}
-
-func getInvocationScript(data []byte, priv *keys.PrivateKey) []byte {
-	signature := priv.Sign(data)
-	buf := io.NewBufBinWriter()
-	emit.Bytes(buf.BinWriter, signature)
-	return buf.Bytes()
 }
 
 // This function generates "../rpc/testdata/testblocks.acc" file which contains data
