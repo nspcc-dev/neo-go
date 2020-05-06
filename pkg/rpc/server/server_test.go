fd72f0635e875f08c1ee816b0a78f0a3a9d3895a
Roman Khimov <roman@nspcc.ru>
2020-05-06 13:10:32 +0300

    rpc/server: prevent test panicing in checkErrGetResult()
    If we expect some resp.Error, then check for it explicitly.

--- pkg/rpc/server/server_test.go
+++ pkg/rpc/server/server_test.go
@@ -1091,6 +1091,7 @@ func checkErrGetResult(t *testing.T, body []byte, expectingFail bool) json.RawMe
 	err := json.Unmarshal(body, &resp)
 	require.Nil(t, err)
 	if expectingFail {
+		require.NotNil(t, resp.Error)
 		assert.NotEqual(t, 0, resp.Error.Code)
 		assert.NotEqual(t, "", resp.Error.Message)
 	} else {
