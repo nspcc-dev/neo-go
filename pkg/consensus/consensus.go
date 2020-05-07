d686fe4e5d2277d83757efc6d7035ff0a07bb2d0
Roman Khimov <roman@nspcc.ru>
2020-05-07 23:00:38 +0300

    network: get blocks directly from the chain for rebroadcasting
    Simplify network<->consensus relations, also broadcast blocks received by
    other means like RPC.

--- pkg/consensus/consensus.go
+++ pkg/consensus/consensus.go
@@ -70,9 +70,6 @@ type Config struct {
 	// Broadcast is a callback which is called to notify server
 	// about new consensus payload to sent.
 	Broadcast func(p *Payload)
-	// RelayBlock is a callback that is called to notify server
-	// about the new block that needs to be broadcasted.
-	RelayBlock func(b *coreb.Block)
 	// Chain is a core.Blockchainer instance.
 	Chain core.Blockchainer
 	// RequestTx is a callback to which will be called
@@ -352,8 +349,6 @@ func (s *service) processBlock(b block.Block) {
 		if _, errget := s.Chain.GetBlock(bb.Hash()); errget != nil {
 			s.log.Warn("error on add block", zap.Error(err))
 		}
-	} else {
-		s.Config.RelayBlock(bb)
 	}
 }
 
