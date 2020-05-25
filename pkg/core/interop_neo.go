nspcc-dev/neo-go

commit 8ed77f0d25e7fec4b1ee2459945d85ada49bf62d

core: make GetUnspentCoins interop return array, fix #978.

pkg/core/interop_neo.go
@@ -7,6 +7,7 @@ import (
 	"math"
 
 	"github.com/nspcc-dev/neo-go/pkg/core/state"
+	"github.com/nspcc-dev/neo-go/pkg/core/storage"
 	"github.com/nspcc-dev/neo-go/pkg/core/transaction"
 	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
 	"github.com/nspcc-dev/neo-go/pkg/smartcontract"
@@ -177,10 +178,20 @@ func (ic *interopContext) txGetUnspentCoins(v *vm.VM) error {
 		return errors.New("value is not a transaction")
 	}
 	ucs, err := ic.dao.GetUnspentCoinState(tx.Hash())
-	if err != nil {
+	if err == storage.ErrKeyNotFound {
+		v.Estack().PushVal([]vm.StackItem{})
+		return nil
+	} else if err != nil {
 		return errors.New("no unspent coin state found")
 	}
-	v.Estack().PushVal(vm.NewInteropItem(ucs))
+
+	items := make([]vm.StackItem, 0, len(ucs.States))
+	for i := range items {
+		if ucs.States[i].State&state.CoinSpent == 0 {
+			items = append(items, vm.NewInteropItem(&ucs.States[i].Output))
+		}
+	}
+	v.Estack().PushVal(items)
 	return nil
 }
 

commit 86ce234d5f3c871f9dfce48f912ce27fe6b544cb

core: fix a typo in GetUnspentCoins interop
Fix a bug introduced in 8ed77f0d.

pkg/core/interop_neo.go
@@ -186,7 +186,7 @@ func (ic *interopContext) txGetUnspentCoins(v *vm.VM) error {
 	}
 
 	items := make([]vm.StackItem, 0, len(ucs.States))
-	for i := range items {
+	for i := range ucs.States {
 		if ucs.States[i].State&state.CoinSpent == 0 {
 			items = append(items, vm.NewInteropItem(&ucs.States[i].Output))
 		}