314430be1d31f26029305b86d4cbd9aff28056c4
Evgenii Stratonikov <evgeniy@nspcc.ru>
2020-06-01 10:44:41 +0300

    mpt: implement NEO storage key conversion

--- pkg/core/mpt/helpers.go
+++ pkg/core/mpt/helpers.go
@@ -1,5 +1,7 @@
 package mpt
 
+import "github.com/nspcc-dev/neo-go/pkg/util"
+
 // lcp returns longest common prefix of a and b.
 // Note: it does no allocations.
 func lcp(a, b []byte) []byte {
@@ -32,4 +34,38 @@ func toNibbles(path []byte) []byte {
 		result[i*2+1] = path[i] & 0x0F
 	}
 	return result
+}
+
+// ToNeoStorageKey converts storage key to C# neo node's format.
+// Key is expected to be at least 20 bytes in length.
+// our format: script hash in BE + key
+// neo format: script hash in LE + key with 0 between every 16 bytes, padded to len 16.
+func ToNeoStorageKey(key []byte) []byte {
+	const groupSize = 16
+
+	var nkey []byte
+	for i := util.Uint160Size - 1; i >= 0; i-- {
+		nkey = append(nkey, key[i])
+	}
+
+	key = key[util.Uint160Size:]
+
+	index := 0
+	remain := len(key)
+	for remain >= groupSize {
+		nkey = append(nkey, key[index:index+groupSize]...)
+		nkey = append(nkey, 0)
+		index += groupSize
+		remain -= groupSize
+	}
+
+	if remain > 0 {
+		nkey = append(nkey, key[index:]...)
+	}
+
+	padding := groupSize - remain
+	for i := 0; i < padding; i++ {
+		nkey = append(nkey, 0)
+	}
+	return append(nkey, byte(padding))
 }
