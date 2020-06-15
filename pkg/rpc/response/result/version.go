79ab3c740ab3c9ae5af7b68fba361a0e73ccf14e
Anna Shaleva <shaleva.ann@nspcc.ru>
2020-06-15 08:05:23 +0300

    rpc: update getversion json fields marshalling
    Following https://github.com/neo-project/neo-modules/pull/262

--- pkg/rpc/response/result/version.go
+++ pkg/rpc/response/result/version.go
@@ -4,9 +4,9 @@ type (
 	// Version model used for reporting server version
 	// info.
 	Version struct {
-		TCPPort   uint16 `json:"tcp_port"`
-		WSPort    uint16 `json:"ws_port,omitempty"`
+		TCPPort   uint16 `json:"tcpPort"`
+		WSPort    uint16 `json:"wsPort,omitempty"`
 		Nonce     uint32 `json:"nonce"`
-		UserAgent string `json:"user_agent"`
+		UserAgent string `json:"userAgent"`
 	}
 )
