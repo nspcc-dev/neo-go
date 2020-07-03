a6fc5cfdf12dab8022b12dd16504c44e652db665
Evgenii Stratonikov <evgeniy@nspcc.ru>
2020-07-03 18:25:18 +0300

    rpc: support raw address in getnep5transfers RPC

--- pkg/rpc/server/server.go
+++ pkg/rpc/server/server.go
@@ -525,7 +525,7 @@ func (s *Server) getNEP5Balances(ps request.Params) (interface{}, *response.Erro
 }
 
 func (s *Server) getNEP5Transfers(ps request.Params) (interface{}, *response.Error) {
-	u, err := ps.ValueWithType(0, request.StringT).GetUint160FromAddress()
+	u, err := ps.Value(0).GetUint160FromAddressOrHex()
 	if err != nil {
 		return nil, response.ErrInvalidParams
 	}
