28e0661f82046d043e9dec6ddce8df3db7764595
Evgenii Stratonikov <evgeniy@nspcc.ru>
2020-07-24 10:46:21 +0300

    interop: update AppCall comment
    Do not require contract hash to be known at compile time.

--- pkg/interop/engine/engine.go
+++ pkg/interop/engine/engine.go
@@ -7,11 +7,7 @@ package engine
 
 // AppCall executes previously deployed blockchain contract with specified hash
 // (160 bit in BE form represented as 20-byte slice) using provided arguments.
-// It returns whatever this contract returns. Even though this function accepts
-// a slice for scriptHash you can only use it for contracts known at
-// compile time, because there is a significant difference between static and
-// dynamic calls in Neo (contracts should have a special property declared
-// and paid for to be able to use dynamic calls). This function uses
+// It returns whatever this contract returns. This function uses
 // `System.Contract.Call` syscall.
 func AppCall(scriptHash []byte, method string, args ...interface{}) interface{} {
 	return nil
