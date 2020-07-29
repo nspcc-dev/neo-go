9a992865b156ce26e4c65d9769e97b86ed404ced
Evgenii Stratonikov <evgeniy@nspcc.ru>
2020-07-29 13:43:29 +0300

    interop: add Secp2k to the list of crypto interops
    Allow to use them during verification.

--- pkg/core/interop/crypto/interop.go
+++ pkg/core/interop/crypto/interop.go
@@ -7,14 +7,18 @@ import (
 
 var (
 	ecdsaSecp256r1VerifyID        = emit.InteropNameToID([]byte("Neo.Crypto.VerifyWithECDsaSecp256r1"))
+	ecdsaSecp256k1VerifyID        = emit.InteropNameToID([]byte("Neo.Crypto.VerifyWithECDsaSecp256k1"))
 	ecdsaSecp256r1CheckMultisigID = emit.InteropNameToID([]byte("Neo.Crypto.CheckMultisigWithECDsaSecp256r1"))
+	ecdsaSecp256k1CheckMultisigID = emit.InteropNameToID([]byte("Neo.Crypto.CheckMultisigWithECDsaSecp256k1"))
 	sha256ID                      = emit.InteropNameToID([]byte("Neo.Crypto.SHA256"))
 	ripemd160ID                   = emit.InteropNameToID([]byte("Neo.Crypto.RIPEMD160"))
 )
 
 var cryptoInterops = []interop.Function{
 	{ID: ecdsaSecp256r1VerifyID, Func: ECDSASecp256r1Verify},
+	{ID: ecdsaSecp256k1VerifyID, Func: ECDSASecp256k1Verify},
 	{ID: ecdsaSecp256r1CheckMultisigID, Func: ECDSASecp256r1CheckMultisig},
+	{ID: ecdsaSecp256k1CheckMultisigID, Func: ECDSASecp256k1CheckMultisig},
 	{ID: sha256ID, Func: Sha256},
 	{ID: ripemd160ID, Func: RipeMD160},
 }
