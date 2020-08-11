9440e9acbc6156bfe06b88224cad0d9926c26bc3
Roman Khimov <roman@nspcc.ru>
2020-08-11 21:12:26 +0300

    integration: drop this old test
    It's not very useful especially given that we have proper neo-bench now.

--- integration/plug.go
+++
@@ -1,6 +0,0 @@
-// Package integration contains integration tests.
-// See README.md for more info.
-// This file exists with the sole purpose to get rid of
-// "go build github.com/nspcc-dev/neo-go/integration: no non-test Go files in *"
-// when running `go test -coverpkg=all`.
-package integration
