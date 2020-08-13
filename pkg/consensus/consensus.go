488ab35421cb66adc39f9c3dd92f0e7574eafb22
Roman Khimov <roman@nspcc.ru>
2020-08-13 16:57:59 +0300

    consensus: move stateroot generation from newBlockFromContext to newCommit
    Fixes #1313. newCommit is only called once and only when we have enough
    signatures unlike newBlockFromContext that is also being called to create
    PrepareResponse.

--- pkg/consensus/consensus.go
+++ pkg/consensus/consensus.go
@@ -172,7 +172,7 @@ func NewService(cfg Config) (Service, error) {
 		dbft.WithNewPrepareRequest(srv.newPrepareRequest),
 		dbft.WithNewPrepareResponse(srv.newPrepareResponse),
 		dbft.WithNewChangeView(func() payload.ChangeView { return new(changeView) }),
-		dbft.WithNewCommit(func() payload.Commit { return new(commit) }),
+		dbft.WithNewCommit(srv.newCommit),
 		dbft.WithNewRecoveryRequest(func() payload.RecoveryRequest { return new(recoveryRequest) }),
 		dbft.WithNewRecoveryMessage(func() payload.RecoveryMessage { return new(recoveryMessage) }),
 		dbft.WithVerifyPrepareRequest(srv.verifyRequest),
@@ -300,6 +300,31 @@ func (s *service) getStateRootSig() []byte {
 		sig, _ = s.dbft.Priv.Sign(data)
 	}
 	return sig
+}
+
+func (s *service) newCommit() payload.Commit {
+	// This is being called when we're ready to commit, so we can safely
+	// relay stateroot here.
+	stateRoot, err := s.Chain.GetStateRoot(s.dbft.Context.BlockIndex - 1)
+	if err != nil {
+		s.log.Warn("can't get stateroot", zap.Uint32("block", s.dbft.Context.BlockIndex-1))
+		return new(commit)
+	}
+	r := stateRoot.MPTRoot
+	r.Witness = s.getWitness(func(ctx dbft.Context, i int) []byte {
+		if p := ctx.PreparationPayloads[i]; p != nil && p.ViewNumber() == ctx.ViewNumber {
+			if int(ctx.PrimaryIndex) == i {
+				return p.GetPrepareRequest().(*prepareRequest).stateRootSig[:]
+			}
+			return p.GetPrepareResponse().(*prepareResponse).stateRootSig[:]
+		}
+		return nil
+	})
+	if err := s.Chain.AddStateRoot(&r); err != nil {
+		s.log.Warn("errors while adding state root", zap.Error(err))
+	}
+	s.Broadcast(&r)
+	return new(commit)
 }
 
 func (s *service) newPrepareResponse() payload.PrepareResponse {
@@ -638,27 +663,6 @@ func convertKeys(validators []crypto.PublicKey) (pubs []*keys.PublicKey) {
 }
 
 func (s *service) newBlockFromContext(ctx *dbft.Context) block.Block {
-	// This is being called when we're ready to commit, so we can safely
-	// relay stateroot here.
-	stateRoot, err := s.Chain.GetStateRoot(s.dbft.Context.BlockIndex - 1)
-	if err != nil {
-		s.log.Warn("can't get stateroot", zap.Uint32("block", s.dbft.Context.BlockIndex-1))
-	}
-	r := stateRoot.MPTRoot
-	r.Witness = s.getWitness(func(ctx dbft.Context, i int) []byte {
-		if p := ctx.PreparationPayloads[i]; p != nil && p.ViewNumber() == ctx.ViewNumber {
-			if int(ctx.PrimaryIndex) == i {
-				return p.GetPrepareRequest().(*prepareRequest).stateRootSig[:]
-			}
-			return p.GetPrepareResponse().(*prepareResponse).stateRootSig[:]
-		}
-		return nil
-	})
-	if err := s.Chain.AddStateRoot(&r); err != nil {
-		s.log.Warn("errors while adding state root", zap.Error(err))
-	}
-	s.Broadcast(&r)
-
 	block := new(neoBlock)
 	if ctx.TransactionHashes == nil {
 		return nil
