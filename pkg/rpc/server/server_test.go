nspcc-dev/neo-go

commit 7e371588a7186a13786abb246395b3f52d066873

core/tx: remove one layer of indirection for scripts and inouts
It reduces heap pressure a little for these elements as we don't have to
allocate/free them individually. And they're directly tied to transactions or
block, not being shared or anything like that, so it makes little sense for
them to be pointer-based. It only makes building transactions a little easier,
but that's obviously a minor usecase.

pkg/core/transaction/transaction.go
@@ -27,18 +27,18 @@ type Transaction struct {
 	Data TXer `json:"-"`
 
 	// Transaction attributes.
-	Attributes []*Attribute `json:"attributes"`
+	Attributes []Attribute `json:"attributes"`
 
 	// The inputs of the transaction.
-	Inputs []*Input `json:"vin"`
+	Inputs []Input `json:"vin"`
 
 	// The outputs of the transaction.
-	Outputs []*Output `json:"vout"`
+	Outputs []Output `json:"vout"`
 
 	// The scripts that comes with this transaction.
 	// Scripts exist out of the verification script
 	// and invocation script.
-	Scripts []*Witness `json:"scripts"`
+	Scripts []Witness `json:"scripts"`
 
 	// Hash of the transaction (double SHA256).
 	hash util.Uint256
@@ -82,12 +82,12 @@ func (t *Transaction) VerificationHash() util.Uint256 {
 
 // AddOutput adds the given output to the transaction outputs.
 func (t *Transaction) AddOutput(out *Output) {
-	t.Outputs = append(t.Outputs, out)
+	t.Outputs = append(t.Outputs, *out)
 }
 
 // AddInput adds the given input to the transaction inputs.
 func (t *Transaction) AddInput(in *Input) {
-	t.Inputs = append(t.Inputs, in)
+	t.Inputs = append(t.Inputs, *in)
 }
 
 // DecodeBinary implements Serializable interface.
@@ -187,8 +187,9 @@ func (t *Transaction) createHash() error {
 // GroupInputsByPrevHash groups all TX inputs by their previous hash.
 func (t *Transaction) GroupInputsByPrevHash() map[util.Uint256][]*Input {
 	m := make(map[util.Uint256][]*Input)
-	for _, in := range t.Inputs {
-		m[in.PrevHash] = append(m[in.PrevHash], in)
+	for i := range t.Inputs {
+		hash := t.Inputs[i].PrevHash
+		m[hash] = append(m[hash], &t.Inputs[i])
 	}
 	return m
 }
@@ -196,8 +197,9 @@ func (t *Transaction) GroupInputsByPrevHash() map[util.Uint256][]*Input {
 // GroupOutputByAssetID groups all TX outputs by their assetID.
 func (t Transaction) GroupOutputByAssetID() map[util.Uint256][]*Output {
 	m := make(map[util.Uint256][]*Output)
-	for _, out := range t.Outputs {
-		m[out.AssetID] = append(m[out.AssetID], out)
+	for i := range t.Outputs {
+		hash := t.Outputs[i].AssetID
+		m[hash] = append(m[hash], &t.Outputs[i])
 	}
 	return m
 }

pkg/consensus/consensus.go
@@ -258,7 +258,7 @@ func (s *service) verifyBlock(b block.Block) bool {
 
 func (s *service) processBlock(b block.Block) {
 	bb := &b.(*neoBlock).Block
-	bb.Script = s.getBlockWitness(bb)
+	bb.Script = *(s.getBlockWitness(bb))
 
 	if err := s.Chain.AddBlock(bb); err != nil {
 		s.log.Warnf("error on add block: %v", err)

pkg/consensus/payload_test.go
@@ -339,10 +339,10 @@ func newMinerTx(nonce uint32) *transaction.Transaction {
 		Data: &transaction.MinerTX{
 			Nonce: rand.Uint32(),
 		},
-		Attributes: []*transaction.Attribute{},
-		Inputs:     []*transaction.Input{},
-		Outputs:    []*transaction.Output{},
-		Scripts:    []*transaction.Witness{},
+		Attributes: []transaction.Attribute{},
+		Inputs:     []transaction.Input{},
+		Outputs:    []transaction.Output{},
+		Scripts:    []transaction.Witness{},
 		Trimmed:    false,
 	}
 }

pkg/core/block.go
@@ -91,7 +91,6 @@ func NewBlockFromTrimmedBytes(b []byte) (*Block, error) {
 	var padding uint8
 	br.ReadLE(&padding)
 
-	block.Script = &transaction.Witness{}
 	block.Script.DecodeBinary(br)
 
 	lenTX := br.ReadVarUint()

pkg/core/block_base.go
@@ -38,7 +38,7 @@ type BlockBase struct {
 	_ uint8
 
 	// Script used to validate the block
-	Script *transaction.Witness `json:"script"`
+	Script transaction.Witness `json:"script"`
 
 	// Hash of this block, created when binary encoded (double SHA256).
 	hash util.Uint256
@@ -80,7 +80,6 @@ func (b *BlockBase) DecodeBinary(br *io.BinReader) {
 		return
 	}
 
-	b.Script = &transaction.Witness{}
 	b.Script.DecodeBinary(br)
 }
 

pkg/core/block_test.go
@@ -87,7 +87,7 @@ func newDumbBlock() *Block {
 			Index:         1,
 			ConsensusData: 1111,
 			NextConsensus: hash.Hash160([]byte("a")),
-			Script: &transaction.Witness{
+			Script: transaction.Witness{
 				VerificationScript: []byte{0x51}, // PUSH1
 				InvocationScript:   []byte{0x61}, // NOP
 			},

pkg/core/blockchain.go
@@ -945,7 +945,7 @@ func (bc *Blockchain) References(t *transaction.Transaction) map[transaction.Inp
 			tx = nil
 		} else if tx != nil {
 			for _, in := range inputs {
-				references[*in] = tx.Outputs[in.PrevIndex]
+				references[*in] = &tx.Outputs[in.PrevIndex]
 			}
 		} else {
 			references = nil
@@ -1243,8 +1243,9 @@ func (bc *Blockchain) GetValidators(txes ...*transaction.Transaction) ([]*keys.P
 
 			// group inputs by the same previous hash and iterate through inputs
 			group := make(map[util.Uint256][]*transaction.Input)
-			for _, input := range tx.Inputs {
-				group[input.PrevHash] = append(group[input.PrevHash], input)
+			for i := range tx.Inputs {
+				hash := tx.Inputs[i].PrevHash
+				group[hash] = append(group[hash], &tx.Inputs[i])
 			}
 
 			for hash, inputs := range group {
@@ -1360,7 +1361,7 @@ func (bc *Blockchain) GetScriptHashesForVerifying(t *transaction.Transaction) ([
 	}
 	hashes := make(map[util.Uint160]bool)
 	for _, i := range t.Inputs {
-		h := references[*i].ScriptHash
+		h := references[i].ScriptHash
 		if _, ok := hashes[h]; !ok {
 			hashes[h] = true
 		}
@@ -1486,7 +1487,7 @@ func (bc *Blockchain) verifyTxWitnesses(t *transaction.Transaction, block *Block
 	sort.Slice(witnesses, func(i, j int) bool { return witnesses[i].ScriptHash().Less(witnesses[j].ScriptHash()) })
 	interopCtx := newInteropContext(trigger.Verification, bc, bc.store, block, t)
 	for i := 0; i < len(hashes); i++ {
-		err := bc.verifyHashAgainstScript(hashes[i], witnesses[i], t.VerificationHash(), interopCtx)
+		err := bc.verifyHashAgainstScript(hashes[i], &witnesses[i], t.VerificationHash(), interopCtx)
 		if err != nil {
 			numStr := fmt.Sprintf("witness #%d", i)
 			return errors.Wrap(err, numStr)
@@ -1505,7 +1506,7 @@ func (bc *Blockchain) verifyBlockWitnesses(block *Block, prevHeader *Header) err
 		hash = prevHeader.NextConsensus
 	}
 	interopCtx := newInteropContext(trigger.Verification, bc, bc.store, nil, nil)
-	return bc.verifyHashAgainstScript(hash, block.Script, block.VerificationHash(), interopCtx)
+	return bc.verifyHashAgainstScript(hash, &block.Script, block.VerificationHash(), interopCtx)
 }
 
 func hashAndIndexToBytes(h util.Uint256, index uint32) []byte {

pkg/core/header_test.go
@@ -20,7 +20,7 @@ func TestHeaderEncodeDecode(t *testing.T) {
 		Index:         3445,
 		ConsensusData: 394949,
 		NextConsensus: util.Uint160{},
-		Script: &transaction.Witness{
+		Script: transaction.Witness{
 			InvocationScript:   []byte{0x10},
 			VerificationScript: []byte{0x11},
 		},

commit 9aee3e5a3448136e7354787671cf416da6cd0198

transaction: set output position when decoding
We had a kludge for getrawtransaction to set this useless field, but
7e371588a7186a13786abb246395b3f52d066873 broke it. Add it right into the
decoder now to fix all types of queries (getblock/getrawtransaction/gettxout).

Fixes #1392.

pkg/core/transaction/transaction.go
@@ -117,6 +117,7 @@ func (t *Transaction) DecodeBinary(br *io.BinReader) {
 			br.Err = errors.New("negative output")
 			return
 		}
+		t.Outputs[i].Position = i
 	}
 	br.ReadArray(&t.Scripts)
 

pkg/rpc/response/result/tx_raw_output.go
@@ -30,10 +30,6 @@ type TransactionMetadata struct {
 func NewTransactionOutputRaw(tx *transaction.Transaction, header *block.Header, chain core.Blockchainer) TransactionOutputRaw {
 	// confirmations formula
 	confirmations := int(chain.BlockHeight() - header.Base.Index + 1)
-	// set index position
-	for i, o := range tx.Outputs {
-		o.Position = i
-	}
 	return TransactionOutputRaw{
 		Transaction: tx,
 		TransactionMetadata: TransactionMetadata{

pkg/rpc/server/server_test.go
@@ -1086,6 +1086,37 @@ func testRPCProtocol(t *testing.T, doRPCCall func(string, string, *testing.T) []
 		assert.Equal(t, TXHash, actual.Transaction.Hash())
 	})
 
+	t.Run("getrawtransaction verbose, check outputs", func(t *testing.T) {
+		block, _ := chain.GetBlock(chain.GetHeaderHash(1))
+		TXHash := block.Transactions[1].Hash()
+		rpc := fmt.Sprintf(`{"jsonrpc": "2.0", "id": 1, "method": "getrawtransaction", "params": ["%s", 1]}"`, TXHash.StringLE())
+		body := doRPCCall(rpc, httpSrv.URL, t)
+		txOut := checkErrGetResult(t, body, false)
+		actual := result.TransactionOutputRaw{}
+		err := json.Unmarshal(txOut, &actual)
+		require.NoErrorf(t, err, "could not parse response: %s", txOut)
+		neoID := core.GoverningTokenID()
+		multiAddr, err := util.Uint160DecodeStringBE("be48d3a3f5d10013ab9ffee489706078714f1ea2")
+		require.NoError(t, err)
+		singleAddr, err := keys.NewPrivateKeyFromWIF("KxyjQ8eUa4FHt3Gvioyt1Wz29cTUrE4eTqX3yFSk1YFCsPL8uNsY")
+		require.NoError(t, err)
+		assert.Equal(t, transaction.ContractType, actual.Transaction.Type)
+		assert.Equal(t, []transaction.Output{
+			{
+				AssetID:    neoID,
+				Amount:     util.Fixed8FromInt64(99999000),
+				ScriptHash: singleAddr.GetScriptHash(),
+				Position:   0,
+			},
+			{
+				AssetID:    neoID,
+				Amount:     util.Fixed8FromInt64(1000),
+				ScriptHash: multiAddr,
+				Position:   1,
+			},
+		}, actual.Transaction.Outputs)
+	})
+
 	t.Run("gettxout", func(t *testing.T) {
 		block, _ := chain.GetBlock(chain.GetHeaderHash(0))
 		tx := block.Transactions[3]