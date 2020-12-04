fc6cb2aa7be09386e5d3cddb7326161c3012580d
Evgenii Stratonikov <evgeniy@nspcc.ru>
2020-12-04 15:26:14 +0300

    network/payload: add missing tests for `InventoryType`

--- pkg/network/payload/inventory_test.go
+++ pkg/network/payload/inventory_test.go
@@ -1,12 +1,14 @@
 package payload
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/nspcc-dev/neo-go/internal/testserdes"
 	"github.com/nspcc-dev/neo-go/pkg/crypto/hash"
 	. "github.com/nspcc-dev/neo-go/pkg/util"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestInventoryEncodeDecode(t *testing.T) {
@@ -26,4 +28,18 @@ func TestEmptyInv(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, []byte{byte(TXType), 0}, data)
 	assert.Equal(t, 0, len(msgInv.Hashes))
+}
+
+func TestValid(t *testing.T) {
+	require.True(t, TXType.Valid())
+	require.True(t, BlockType.Valid())
+	require.True(t, ConsensusType.Valid())
+	require.False(t, InventoryType(0xFF).Valid())
+}
+
+func TestString(t *testing.T) {
+	require.Equal(t, "TX", TXType.String())
+	require.Equal(t, "block", BlockType.String())
+	require.Equal(t, "consensus", ConsensusType.String())
+	require.True(t, strings.Contains(InventoryType(0xFF).String(), "unknown"))
 }
