bb316f4652333679bbb240d3f2d21ad997dfa671
Evgenii Stratonikov <evgeniy@nspcc.ru>
2020-12-04 11:03:39 +0300

    cli/test: trim newline char in `checkNextLine`

--- cli/executor_test.go
+++ cli/executor_test.go
@@ -132,7 +132,7 @@ func (e *executor) GetTransaction(t *testing.T, h util.Uint256) (*transaction.Tr
 func (e *executor) getNextLine(t *testing.T) string {
 	line, err := e.Out.ReadString('\n')
 	require.NoError(t, err)
-	return line
+	return strings.TrimSuffix(line, "\n")
 }
 
 func (e *executor) checkNextLine(t *testing.T, expected string) {
