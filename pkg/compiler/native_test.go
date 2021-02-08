2f26490e5903bd6ba082795b6cf749ce4f3e2d1c
Evgeniy Stratonikov <evgeniy@nspcc.ru>
2021-02-08 11:08:48 +0300

    compiler: implement RoleManagement contract wrapper

--- pkg/compiler/native_test.go
+++ pkg/compiler/native_test.go
@@ -5,8 +5,15 @@ import (
 
 	"github.com/nspcc-dev/neo-go/pkg/core/native"
 	"github.com/nspcc-dev/neo-go/pkg/interop/native/nameservice"
+	"github.com/nspcc-dev/neo-go/pkg/interop/native/roles"
 	"github.com/stretchr/testify/require"
 )
+
+func TestRoleManagementRole(t *testing.T) {
+	require.EqualValues(t, native.RoleOracle, roles.Oracle)
+	require.EqualValues(t, native.RoleStateValidator, roles.StateValidator)
+	require.EqualValues(t, native.RoleP2PNotary, roles.P2PNotary)
+}
 
 func TestNameServiceRecordType(t *testing.T) {
 	require.EqualValues(t, native.RecordTypeA, nameservice.TypeA)
