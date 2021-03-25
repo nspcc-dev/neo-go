4c4a980cc018ac1401d4696e163a71efd4e44f65
Roman Khimov <roman@nspcc.ru>
2021-03-25 22:42:20 +0300

    crypto: add Hashable to VerifiableDecodable
    It has to be Hashable.

--- pkg/crypto/verifiable.go
+++ pkg/crypto/verifiable.go
@@ -1,8 +1,11 @@
 package crypto
 
+import "github.com/nspcc-dev/neo-go/pkg/crypto/hash"
+
 // VerifiableDecodable represents an object which can be verified and
 // those hashable part can be encoded/decoded.
 type VerifiableDecodable interface {
+	hash.Hashable
 	EncodeHashableFields() ([]byte, error)
 	DecodeHashableFields([]byte) error
 }
