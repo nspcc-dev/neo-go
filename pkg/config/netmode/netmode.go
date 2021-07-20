f9a9d15490d0e21f57c45b1438d2b2d3e7f2086b
Roman Khimov <roman@nspcc.ru>
2021-07-20 12:55:45 +0300

    config: update testnet magic for RC4
    See neo-project/neo-node#798 and https://github.com/neo-project/neo-node/releases/tag/v3.0.0-rc4

--- pkg/config/netmode/netmode.go
+++ pkg/config/netmode/netmode.go
@@ -6,7 +6,7 @@ const (
 	// MainNet contains magic code used in the NEO main official network.
 	MainNet Magic = 0x004f454e // 5195086
 	// TestNet contains magic code used in the NEO testing network.
-	TestNet Magic = 0x3254334e // N3T2
+	TestNet Magic = 0x3454334e // N3T4
 	// PrivNet contains magic code usually used for NEO private networks.
 	PrivNet Magic = 56753 // docker privnet
 	// UnitTestNet is a stub magic code used for testing purposes.
