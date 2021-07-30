b974b41be231ce615bfaeb3722e2c4225b6d2d3c
Evgeniy Stratonikov <evgeniy@nspcc.ru>
2021-07-30 15:10:28 +0300

    io: optimize `GetVarSize`
    1. Use reflection only when strictly needed.
    2. Cast to `encodable` instead of `Serializable`.
    3. Use Specialized `BinaryWriter` implementation for counting.
    
    Results for `GetVarSize` of transfer transaction.
    ```
    name           old time/op  new time/op  delta
    VarSize-8       726ns ± 5%   541ns ± 6%  -25.49%  (p=0.000 n=10+10)
    ```
    
    Signed-off-by: Evgeniy Stratonikov <evgeniy@nspcc.ru>

--- pkg/io/size.go
+++ pkg/io/size.go
@@ -10,13 +10,63 @@ import (
 // just counts the number of bytes to be written.
 type counterWriter struct {
 	counter int
+	err     error
 }
 
-// Write implements the io.Writer interface.
-func (cw *counterWriter) Write(p []byte) (int, error) {
-	n := len(p)
-	cw.counter += n
-	return n, nil
+var _ BinaryWriter = (*counterWriter)(nil)
+
+func (cw *counterWriter) WriteU64LE(uint64) {
+	cw.counter += 8
+}
+
+func (cw *counterWriter) WriteU32LE(uint32) {
+	cw.counter += 4
+}
+
+func (cw *counterWriter) WriteU16LE(u16 uint16) {
+	cw.counter += 2
+}
+
+func (cw *counterWriter) WriteU16BE(u16 uint16) {
+	cw.counter += 2
+}
+
+func (cw *counterWriter) WriteB(u8 byte) {
+	cw.counter++
+}
+
+func (cw *counterWriter) WriteBool(b bool) {
+	cw.counter++
+}
+
+func (cw *counterWriter) WriteArray(arr interface{}) {
+	writeArray(cw, arr)
+}
+
+func (cw *counterWriter) WriteVarUint(val uint64) {
+	cw.counter += GetVarSize(val)
+}
+
+func (cw *counterWriter) WriteBytes(b []byte) {
+	cw.counter += len(b)
+}
+
+func (cw *counterWriter) WriteVarBytes(b []byte) {
+	cw.WriteVarUint(uint64(len(b)))
+	cw.counter += len(b)
+}
+
+func (cw *counterWriter) WriteString(s string) {
+	cw.WriteVarUint(uint64(len(s)))
+	cw.counter += len(s)
+}
+
+func (cw *counterWriter) Error() error {
+	return cw.err
+}
+
+func (cw *counterWriter) SetError(err error) {
+	cw.err = err
 }
 
 // getVarIntSize returns the size in number of bytes of a variable integer.
@@ -39,45 +89,54 @@ func getVarIntSize(value int) int {
 // slices and arrays of ints/uints or Serializable structures. It's similar to GetVarSize<T>(this T[] value)
 // used in C#, but differs in that it also supports things like Uint160 or Uint256.
 func GetVarSize(value interface{}) int {
-	v := reflect.ValueOf(value)
-	switch v.Kind() {
-	case reflect.String:
-		valueSize := len([]byte(v.String()))
+	switch v := value.(type) {
+	case int:
+		return getVarIntSize(int(v))
+	case int8:
+		return getVarIntSize(int(v))
+	case int16:
+		return getVarIntSize(int(v))
+	case int32:
+		return getVarIntSize(int(v))
+	case int64:
+		return getVarIntSize(int(v))
+	case uint:
+		return getVarIntSize(int(v))
+	case uint8:
+		return getVarIntSize(int(v))
+	case uint16:
+		return getVarIntSize(int(v))
+	case uint32:
+		return getVarIntSize(int(v))
+	case uint64:
+		return getVarIntSize(int(v))
+	case string:
+		valueSize := len(v)
 		return getVarIntSize(valueSize) + valueSize
-	case reflect.Int,
-		reflect.Int8,
-		reflect.Int16,
-		reflect.Int32,
-		reflect.Int64:
-		return getVarIntSize(int(v.Int()))
-	case reflect.Uint,
-		reflect.Uint8,
-		reflect.Uint16,
-		reflect.Uint32,
-		reflect.Uint64:
-		return getVarIntSize(int(v.Uint()))
-	case reflect.Ptr:
-		vser, ok := v.Interface().(Serializable)
-		if !ok {
-			panic("unable to calculate GetVarSize for a non-Serializable pointer")
-		}
+	case encodable:
 		cw := counterWriter{}
-		w := NewBinWriterFromIO(&cw)
-		vser.EncodeBinary(w)
-		if w.Err != nil {
-			panic(fmt.Sprintf("error serializing %s: %s", reflect.TypeOf(value), w.Err.Error()))
+		v.EncodeBinary(&cw)
+		if cw.err != nil {
+			panic(fmt.Sprintf("error serializing %s: %s", reflect.TypeOf(value), cw.err))
 		}
 		return cw.counter
+	}
+
+	v := reflect.ValueOf(value)
+	switch v.Kind() {
 	case reflect.Slice, reflect.Array:
 		valueLength := v.Len()
 		valueSize := 0
 
 		if valueLength != 0 {
 			switch reflect.ValueOf(value).Index(0).Interface().(type) {
-			case Serializable:
-				for i := 0; i < valueLength; i++ {
-					valueSize += GetVarSize(v.Index(i).Interface())
+			case encodable:
+				var cw counterWriter
+				cw.WriteArray(value)
+				if cw.err != nil {
+					panic(fmt.Errorf("invalid: %w", cw.err))
 				}
+				return cw.counter
 			case uint8, int8:
 				valueSize = valueLength
 			case uint16, int16:
--- pkg/io/size_test.go
+++ pkg/io/size_test.go
@@ -29,159 +29,170 @@ func (*smthNotReallySerializable) EncodeBinary(bw io.BinaryWriter) {
 	bw.SetError(fmt.Errorf("smth bad happened in smthNotReallySerializable"))
 }
 
-func TestVarSize(t *testing.T) {
-	testCases := []struct {
-		variable interface{}
-		name     string
-		expected int
-	}{
-		{
-			252,
-			"test_int_1",
-			1,
-		},
-		{
-			253,
-			"test_int_2",
-			3,
-		},
-		{
-			65535,
-			"test_int_3",
-			3,
-		},
-		{
-			65536,
-			"test_int_4",
-			5,
-		},
-		{
-			4294967295,
-			"test_int_5",
-			5,
-		},
-		{
-			uint(252),
-			"test_uint_1",
-			1,
-		},
-		{
-			uint(253),
-			"test_uint_2",
-			3,
-		},
-		{
-			uint(65535),
-			"test_uint_3",
-			3,
-		},
-		{
-			uint(65536),
-			"test_uint_4",
-			5,
-		},
-		{
-			uint(4294967295),
-			"test_uint_5",
-			5,
-		},
-		{
-			[]byte{1, 2, 4, 5, 6},
-			"test_[]byte_1",
-			6,
-		},
-		{
-			// The neo C# implementation doe not allowed this!
-			util.Uint160{1, 2, 4, 5, 6},
-			"test_Uint160_1",
-			21,
-		},
-
-		{[20]uint8{1, 2, 3, 4, 5, 6},
-			"test_uint8_1",
-			21,
-		},
-		{[20]uint8{1, 2, 3, 4, 5, 6, 8, 9},
-			"test_uint8_2",
-			21,
-		},
-
-		{[32]uint8{1, 2, 3, 4, 5, 6},
-			"test_uint8_3",
-			33,
-		},
-		{[10]uint16{1, 2, 3, 4, 5, 6},
-			"test_uint16_1",
-			21,
-		},
-
-		{[10]uint16{1, 2, 3, 4, 5, 6, 10, 21},
-			"test_uint16_2",
-			21,
-		},
-		{[30]uint32{1, 2, 3, 4, 5, 6, 10, 21},
-			"test_uint32_2",
-			121,
-		},
-		{[30]uint64{1, 2, 3, 4, 5, 6, 10, 21},
-			"test_uint64_2",
-			241,
-		},
-		{[20]int8{1, 2, 3, 4, 5, 6},
-			"test_int8_1",
-			21,
-		},
-		{[20]int8{-1, 2, 3, 4, 5, 6, 8, 9},
-			"test_int8_2",
-			21,
-		},
-
-		{[32]int8{-1, 2, 3, 4, 5, 6},
-			"test_int8_3",
-			33,
-		},
-		{[10]int16{-1, 2, 3, 4, 5, 6},
-			"test_int16_1",
-			21,
-		},
-
-		{[10]int16{-1, 2, 3, 4, 5, 6, 10, 21},
-			"test_int16_2",
-			21,
-		},
-		{[30]int32{-1, 2, 3, 4, 5, 6, 10, 21},
-			"test_int32_2",
-			121,
-		},
-		{[30]int64{-1, 2, 3, 4, 5, 6, 10, 21},
-			"test_int64_2",
-			241,
-		},
+var sizeTestCases = []struct {
+	variable interface{}
+	name     string
+	expected int
+}{
+	{
+		252,
+		"test_int_1",
+		1,
+	},
+	{
+		253,
+		"test_int_2",
+		3,
+	},
+	{
+		65535,
+		"test_int_3",
+		3,
+	},
+	{
+		65536,
+		"test_int_4",
+		5,
+	},
+	{
+		4294967295,
+		"test_int_5",
+		5,
+	},
+	{
+		uint(252),
+		"test_uint_1",
+		1,
+	},
+	{
+		uint(253),
+		"test_uint_2",
+		3,
+	},
+	{
+		uint(65535),
+		"test_uint_3",
+		3,
+	},
+	{
+		uint(65536),
+		"test_uint_4",
+		5,
+	},
+	{
+		uint(4294967295),
+		"test_uint_5",
+		5,
+	},
+	{
+		[]byte{1, 2, 4, 5, 6},
+		"test_[]byte_1",
+		6,
+	},
+	{
 		// The neo C# implementation doe not allowed this!
-		{util.Uint256{1, 2, 3, 4, 5, 6},
-			"test_Uint256_1",
-			33,
-		},
+		util.Uint160{1, 2, 4, 5, 6},
+		"test_Uint160_1",
+		21,
+	},
 
-		{"abc",
-			"test_string_1",
-			4,
-		},
-		{"abcà",
-			"test_string_2",
-			6,
-		},
-		{"2d3b96ae1bcc5a585e075e3b81920210dec16302",
-			"test_string_3",
-			41,
-		},
-		{[]*smthSerializable{{}, {}},
-			"test_Serializable",
-			2*42 + 1,
-		},
+	{[20]uint8{1, 2, 3, 4, 5, 6},
+		"test_uint8_1",
+		21,
+	},
+	{[20]uint8{1, 2, 3, 4, 5, 6, 8, 9},
+		"test_uint8_2",
+		21,
+	},
+
+	{[32]uint8{1, 2, 3, 4, 5, 6},
+		"test_uint8_3",
+		33,
+	},
+	{[10]uint16{1, 2, 3, 4, 5, 6},
+		"test_uint16_1",
+		21,
+	},
+
+	{[10]uint16{1, 2, 3, 4, 5, 6, 10, 21},
+		"test_uint16_2",
+		21,
+	},
+	{[30]uint32{1, 2, 3, 4, 5, 6, 10, 21},
+		"test_uint32_2",
+		121,
+	},
+	{[30]uint64{1, 2, 3, 4, 5, 6, 10, 21},
+		"test_uint64_2",
+		241,
+	},
+	{[20]int8{1, 2, 3, 4, 5, 6},
+		"test_int8_1",
+		21,
+	},
+	{[20]int8{-1, 2, 3, 4, 5, 6, 8, 9},
+		"test_int8_2",
+		21,
+	},
+
+	{[32]int8{-1, 2, 3, 4, 5, 6},
+		"test_int8_3",
+		33,
+	},
+	{[10]int16{-1, 2, 3, 4, 5, 6},
+		"test_int16_1",
+		21,
+	},
+
+	{[10]int16{-1, 2, 3, 4, 5, 6, 10, 21},
+		"test_int16_2",
+		21,
+	},
+	{[30]int32{-1, 2, 3, 4, 5, 6, 10, 21},
+		"test_int32_2",
+		121,
+	},
+	{[30]int64{-1, 2, 3, 4, 5, 6, 10, 21},
+		"test_int64_2",
+		241,
+	},
+	// The neo C# implementation doe not allowed this!
+	{util.Uint256{1, 2, 3, 4, 5, 6},
+		"test_Uint256_1",
+		33,
+	},
+
+	{"abc",
+		"test_string_1",
+		4,
+	},
+	{"abcà",
+		"test_string_2",
+		6,
+	},
+	{"2d3b96ae1bcc5a585e075e3b81920210dec16302",
+		"test_string_3",
+		41,
+	},
+	{[]*smthSerializable{{}, {}},
+		"test_Serializable",
+		2*42 + 1,
+	},
+}
+
+func BenchmarkGetVarSize(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		for i := range sizeTestCases {
+			if sizeTestCases[i].expected != io.GetVarSize(sizeTestCases[i].variable) {
+				b.FailNow()
+			}
+		}
 	}
+}
 
-	for _, tc := range testCases {
+func TestVarSize(t *testing.T) {
+	for _, tc := range sizeTestCases {
 		t.Run(fmt.Sprintf("run: %s", tc.name), func(t *testing.T) {
 			result := io.GetVarSize(tc.variable)
 			assert.Equal(t, tc.expected, result)
