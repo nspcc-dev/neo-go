eb07a4cfc6d70f934bc8a9d2bb1deadc537ae81d
Evgeniy Stratonikov <evgeniy@nspcc.ru>
2021-10-16 15:25:55 +0300

    .docker: fix shellcheck issues
    Signed-off-by: Evgeniy Stratonikov <evgeniy@nspcc.ru>

--- .docker/privnet-entrypoint.sh
+++ .docker/privnet-entrypoint.sh
@@ -3,13 +3,13 @@
 BIN=/usr/bin/neo-go
 
 case $@ in
-  "node"*)
-  echo "=> Try to restore blocks before running node"
-  if [ -n "$ACC" -a -f "$ACC" ]; then
-    gunzip --stdout "$ACC" > /privnet.acc
-    ${BIN} db restore -p --config-path /config -i /privnet.acc
-  fi
-    ;;
+"node"*)
+	echo "=> Try to restore blocks before running node"
+	if [ -f "$ACC" ]; then
+		gunzip --stdout "$ACC" >/privnet.acc
+		${BIN} db restore -p --config-path /config -i /privnet.acc
+	fi
+	;;
 esac
 
 ${BIN} "$@"
