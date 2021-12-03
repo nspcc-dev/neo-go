7927c8d8f50cbb48f17f79e44067836b4236419d
Anna Shaleva <anna@nspcc.ru>
2021-12-03 18:51:46 +0300

    nativetest: migrate Designation contract tests to neotest

--- pkg/core/native/native_test/common_test.go
+++ pkg/core/native/native_test/common_test.go
@@ -4,9 +4,15 @@ import (
 	"math/big"
 	"testing"
 
+	"github.com/nspcc-dev/neo-go/pkg/core/native"
+	"github.com/nspcc-dev/neo-go/pkg/core/native/noderoles"
+	"github.com/nspcc-dev/neo-go/pkg/core/state"
+	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
+
 	"github.com/nspcc-dev/neo-go/pkg/neotest"
 	"github.com/nspcc-dev/neo-go/pkg/neotest/chain"
 	"github.com/nspcc-dev/neo-go/pkg/vm/stackitem"
+	"github.com/stretchr/testify/require"
 )
 
 func newNativeClient(t *testing.T, name string) *neotest.ContractInvoker {
@@ -60,4 +66,39 @@ func testGetSet(t *testing.T, c *neotest.ContractInvoker, name string, defaultVa
 		// Get in the next block.
 		randomInvoker.Invoke(t, defaultValue+1, getName)
 	})
+}
+
+func setNodesByRole(t *testing.T, designateInvoker *neotest.ContractInvoker, ok bool, r noderoles.Role, nodes keys.PublicKeys) {
+	pubs := make([]interface{}, len(nodes))
+	for i := range nodes {
+		pubs[i] = nodes[i].Bytes()
+	}
+	if ok {
+		h := designateInvoker.Invoke(t, stackitem.Null{}, "designateAsRole", int64(r), pubs)
+		designateInvoker.CheckTxNotificationEvent(t, h, 0, state.NotificationEvent{
+			ScriptHash: designateInvoker.Hash,
+			Name:       native.DesignationEventName,
+			Item: stackitem.NewArray([]stackitem.Item{
+				stackitem.Make(int64(r)),
+				stackitem.Make(designateInvoker.Chain.BlockHeight()),
+			}),
+		})
+	} else {
+		designateInvoker.InvokeFail(t, "", "designateAsRole", int64(r), pubs)
+	}
+}
+
+func checkNodeRoles(t *testing.T, designateInvoker *neotest.ContractInvoker, ok bool, r noderoles.Role, index uint32, res keys.PublicKeys) {
+	if ok {
+		designateInvoker.InvokeAndCheck(t, func(t *testing.T, stack []stackitem.Item) {
+			require.Equal(t, 1, len(stack))
+			arr := stack[0].Value().([]stackitem.Item)
+			require.Equal(t, len(res), len(arr))
+			for i := range arr {
+				require.Equal(t, res[i].Bytes(), arr[i].Value().([]byte), i)
+			}
+		}, "getDesignatedByRole", int64(r), int64(index))
+	} else {
+		designateInvoker.InvokeFail(t, "", "getDesignatedByRole", int64(r), int64(index))
+	}
 }
