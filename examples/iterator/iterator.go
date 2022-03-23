3070c2e7fceec95fbc6b00d8b99c58ac3ad6d4ce
Roman Khimov <roman@nspcc.ru>
2022-03-23 22:05:40 +0300

    examples: polish iterator
    Use read only context where appropriate, shorten notifications, fix comment.

--- examples/iterator/iterator.go
+++ examples/iterator/iterator.go
@@ -8,24 +8,24 @@ import (
 
 // _deploy primes contract's storage with some data to be used later.
 func _deploy(_ interface{}, _ bool) {
-	ctx := storage.GetContext()
+	ctx := storage.GetContext() // RW context.
 	storage.Put(ctx, "foo1", "1")
 	storage.Put(ctx, "foo2", "2")
 	storage.Put(ctx, "foo3", "3")
 }
 
-// NotifyKeysAndValues sends notification with `foo` storage keys and values
+// NotifyKeysAndValues sends notification with `foo` storage keys and values.
 func NotifyKeysAndValues() bool {
-	iter := storage.Find(storage.GetContext(), []byte("foo"), storage.None)
+	iter := storage.Find(storage.GetReadOnlyContext(), []byte("foo"), storage.None)
 	for iterator.Next(iter) {
-		runtime.Notify("found storage key-value pair", iterator.Value(iter))
+		runtime.Notify("Key-Value", iterator.Value(iter))
 	}
 	return true
 }
 
 // NotifyValues sends notification with `foo` storage values.
 func NotifyValues() bool {
-	iter := storage.Find(storage.GetContext(), []byte("foo"), storage.ValuesOnly)
+	iter := storage.Find(storage.GetReadOnlyContext(), []byte("foo"), storage.ValuesOnly)
 	for iterator.Next(iter) {
 		runtime.Notify("Value", iterator.Value(iter))
 	}
