4c4e9c03717c7c3d866d55873f91801af742c9d6
Roman Khimov <roman@nspcc.ru>
2022-03-23 22:02:19 +0300

    examples: add something to the storage for iterator example

--- examples/iterator/iterator.go
+++ examples/iterator/iterator.go
@@ -6,6 +6,14 @@ import (
 	"github.com/nspcc-dev/neo-go/pkg/interop/storage"
 )
 
+// _deploy primes contract's storage with some data to be used later.
+func _deploy(_ interface{}, _ bool) {
+	ctx := storage.GetContext()
+	storage.Put(ctx, "foo1", "1")
+	storage.Put(ctx, "foo2", "2")
+	storage.Put(ctx, "foo3", "3")
+}
+
 // NotifyKeysAndValues sends notification with `foo` storage keys and values
 func NotifyKeysAndValues() bool {
 	iter := storage.Find(storage.GetContext(), []byte("foo"), storage.None)
