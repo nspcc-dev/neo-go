34b1b52784b808eff826d1258ef8fd919666de26
Evgeniy Stratonikov <evgeniy@nspcc.ru>
2022-03-24 17:15:43 +0300

    network: check compressed payload size in `decompress`
    Signed-off-by: Evgeniy Stratonikov <evgeniy@nspcc.ru>

---
+++ pkg/network/fuzz_test.go
@@ -0,0 +1,26 @@
+//go:build go1.18
+// +build go1.18
+
+package network
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/nspcc-dev/neo-go/pkg/io"
+	"github.com/stretchr/testify/require"
+)
+
+func FuzzMessageDecode(f *testing.F) {
+	for i := 0; i < 100; i++ {
+		seed := make([]byte, rand.Uint32()%1000)
+		rand.Read(seed)
+		f.Add(seed)
+	}
+
+	f.Fuzz(func(t *testing.T, value []byte) {
+		m := new(Message)
+		r := io.NewBinReaderFromBuf(value)
+		require.NotPanics(t, func() { _ = m.Decode(r) })
+	})
+}
