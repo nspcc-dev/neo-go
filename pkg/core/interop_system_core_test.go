ca127f161596c2d7b26f9fe20b526ae648333986
Anna Shaleva <shaleva.ann@nspcc.ru>
2022-06-02 08:00:03 +0300

    core: fix native NEO ABI
    This commit fixes T5 statediff at block #0. The reason in Management's
    storage state. The diff occurs because of inconsistent NEO methods
    order. See
    https://github.com/neo-project/neo/issues/2766#issue-1257870089.
    
    The current solution is to preserve C#'s order of methods to be
    compatible with current testnet.
    
    See also
    https://docs.microsoft.com/ru-ru/dotnet/api/system.stringcomparer?view=net-6.0
    and
    https://stackoverflow.com/questions/28638714/easiest-method-to-orderby-a-string-using-stringcomparison-ordinal
    for more details.

--- pkg/core/interop_system_core_test.go
+++ pkg/core/interop_system_core_test.go
@@ -967,3 +967,18 @@ func TestRuntimeCheckWitness(t *testing.T) {
 		})
 	})
 }
+
+// TestNativeGetMethod is needed to ensure that methods list has the same sorting
+// rule as we expect inside the `ContractMD.GetMethod`.
+func TestNativeGetMethod(t *testing.T) {
+	cfg := config.ProtocolConfiguration{P2PSigExtensions: true}
+	cs := native.NewContracts(cfg)
+	for _, c := range cs.Contracts {
+		t.Run(c.Metadata().Name, func(t *testing.T) {
+			for _, m := range c.Metadata().Methods {
+				_, ok := c.Metadata().GetMethod(m.MD.Name, len(m.MD.Parameters))
+				require.True(t, ok)
+			}
+		})
+	}
+}
