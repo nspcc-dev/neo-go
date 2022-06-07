19ad31dc527f1c8fbd152a68f8f2b0cf4ccd7fbc
Roman Khimov <roman@nspcc.ru>
2022-06-07 10:29:13 +0300

    vm: optimize IsSignatureContract
    We use it a lot in (*Blockchain).IsTxStillRelevant().
    
    name                   old time/op    new time/op    delta
    IsSignatureContract-8    19.1ns ± 5%     1.2ns ± 4%  -93.81%  (p=0.000 n=10+10)
    
    name                   old alloc/op   new alloc/op   delta
    IsSignatureContract-8     0.00B          0.00B          ~     (all equal)
    
    name                   old allocs/op  new allocs/op  delta
    IsSignatureContract-8      0.00           0.00          ~     (all equal)

--- pkg/vm/bench_test.go
+++ pkg/vm/bench_test.go
@@ -48,3 +48,12 @@ func BenchmarkScriptPushPop(t *testing.B) {
 		})
 	}
 }
+
+func BenchmarkIsSignatureContract(t *testing.B) {
+	b64script := "DCED2eixa9myLTNF1tTN4xvhw+HRYVMuPQzOy5Xs4utYM25BVuezJw=="
+	script, err := base64.StdEncoding.DecodeString(b64script)
+	require.NoError(t, err)
+	for n := 0; n < t.N; n++ {
+		_ = IsSignatureContract(script)
+	}
+}
