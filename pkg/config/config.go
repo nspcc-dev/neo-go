nspcc-dev/neo-go

commit 54c2aa858295a38f79b1bf375a79e48194d4ceb9

config: move P2P options to a separate config section
And convert time-related settings to a Duration format along the way.

pkg/config/config.go
@@ -55,8 +55,10 @@ func LoadFile(configPath string) (Config, error) {
 
 	config := Config{
 		ApplicationConfiguration: ApplicationConfiguration{
-			PingInterval: 30,
-			PingTimeout:  90,
+			P2P: P2P{
+				PingInterval: 30,
+				PingTimeout:  30,
+			},
 			RPC: RPC{
 				MaxIteratorResultItems: DefaultMaxIteratorResultItems,
 				MaxFindResultItems:     100,

config/protocol.mainnet.neofs.yml
@@ -47,16 +47,17 @@ ApplicationConfiguration:
       DataDirectoryPath: "./chains/mainnet.neofs"
   #    BoltDBOptions:
   #      FilePath: "./chains/mainnet.bolt"
-  Addresses:
-    - ":40333" # in form of "[host]:[port][:announcedPort]"
+  P2P:
+    Addresses:
+      - ":40333" # in form of "[host]:[port][:announcedPort]"
+    DialTimeout: 3s
+    ProtoTickInterval: 2s
+    PingInterval: 30s
+    PingTimeout: 90s
+    MaxPeers: 100
+    AttemptConnPeers: 20
+    MinPeers: 5
   Relay: true
-  DialTimeout: 3
-  ProtoTickInterval: 2
-  PingInterval: 30
-  PingTimeout: 90
-  MaxPeers: 100
-  AttemptConnPeers: 20
-  MinPeers: 5
   Oracle:
     Enabled: false
     AllowedContentTypes:

config/protocol.mainnet.yml
@@ -59,16 +59,17 @@ ApplicationConfiguration:
       DataDirectoryPath: "./chains/mainnet"
   #    BoltDBOptions:
   #      FilePath: "./chains/mainnet.bolt"
-  Addresses:
-    - ":10333" # in form of "[host]:[port][:announcedPort]"
+  P2P:
+    Addresses:
+      - ":10333" # in form of "[host]:[port][:announcedPort]"
+    DialTimeout: 3s
+    ProtoTickInterval: 2s
+    PingInterval: 30s
+    PingTimeout: 90s
+    MaxPeers: 100
+    AttemptConnPeers: 20
+    MinPeers: 10
   Relay: true
-  DialTimeout: 3
-  ProtoTickInterval: 2
-  PingInterval: 30
-  PingTimeout: 90
-  MaxPeers: 100
-  AttemptConnPeers: 20
-  MinPeers: 10
   Oracle:
     Enabled: false
     AllowedContentTypes:

config/protocol.privnet.docker.four.yml
@@ -38,17 +38,17 @@ ApplicationConfiguration:
       DataDirectoryPath: "/chains/four"
   #    BoltDBOptions:
   #      FilePath: "./chains/privnet.bolt"
-  #  Uncomment in order to set up custom address for node.
-  Addresses:
-    - ":20336" # in form of "[host]:[port][:announcedPort]"
+  P2P:
+    Addresses:
+      - ":20336" # in form of "[host]:[port][:announcedPort]"
+    DialTimeout: 3s
+    ProtoTickInterval: 2s
+    PingInterval: 30s
+    PingTimeout: 90s
+    MaxPeers: 10
+    AttemptConnPeers: 5
+    MinPeers: 3
   Relay: true
-  DialTimeout: 3
-  ProtoTickInterval: 2
-  PingInterval: 30
-  PingTimeout: 90
-  MaxPeers: 10
-  AttemptConnPeers: 5
-  MinPeers: 3
   Oracle:
     Enabled: false
     AllowedContentTypes:

config/protocol.privnet.docker.one.yml
@@ -38,16 +38,17 @@ ApplicationConfiguration:
       DataDirectoryPath: "/chains/one"
   #    BoltDBOptions:
   #      FilePath: "./chains/privnet.bolt"
-  Addresses:
-    - ":20333" # in form of "[host]:[port][:announcedPort]"
+  P2P:
+    Addresses:
+      - ":20333" # in form of "[host]:[port][:announcedPort]"
+    DialTimeout: 3s
+    ProtoTickInterval: 2s
+    PingInterval: 30s
+    PingTimeout: 90s
+    MaxPeers: 10
+    AttemptConnPeers: 5
+    MinPeers: 3
   Relay: true
-  DialTimeout: 3
-  ProtoTickInterval: 2
-  PingInterval: 30
-  PingTimeout: 90
-  MaxPeers: 10
-  AttemptConnPeers: 5
-  MinPeers: 3
   Oracle:
     Enabled: false
     AllowedContentTypes:

config/protocol.privnet.docker.single.yml
@@ -32,16 +32,17 @@ ApplicationConfiguration:
       DataDirectoryPath: "/chains/single"
   #    BoltDBOptions:
   #      FilePath: "./chains/privnet.bolt"
-  Addresses:
-    - ":20333" # in form of "[host]:[port][:announcedPort]"
+  P2P:
+    Addresses:
+      - ":20333" # in form of "[host]:[port][:announcedPort]"
+    DialTimeout: 3s
+    ProtoTickInterval: 2s
+    PingInterval: 30s
+    PingTimeout: 90s
+    MaxPeers: 10
+    AttemptConnPeers: 5
+    MinPeers: 0
   Relay: true
-  DialTimeout: 3
-  ProtoTickInterval: 2
-  PingInterval: 30
-  PingTimeout: 90
-  MaxPeers: 10
-  AttemptConnPeers: 5
-  MinPeers: 0
   Oracle:
     Enabled: false
     AllowedContentTypes:

config/protocol.privnet.docker.three.yml
@@ -38,16 +38,17 @@ ApplicationConfiguration:
       DataDirectoryPath: "/chains/three"
   #    BoltDBOptions:
   #      FilePath: "./chains/privnet.bolt"
-  Addresses:
-    - ":20335" # in form of "[host]:[port][:announcedPort]"
+  P2P:
+    Addresses:
+      - ":20335" # in form of "[host]:[port][:announcedPort]"
+    DialTimeout: 3s
+    ProtoTickInterval: 2s
+    PingInterval: 30s
+    PingTimeout: 90s
+    MaxPeers: 10
+    AttemptConnPeers: 5
+    MinPeers: 3
   Relay: true
-  DialTimeout: 3
-  ProtoTickInterval: 2
-  PingInterval: 30
-  PingTimeout: 90
-  MaxPeers: 10
-  AttemptConnPeers: 5
-  MinPeers: 3
   Oracle:
     Enabled: false
     AllowedContentTypes:

config/protocol.privnet.docker.two.yml
@@ -38,16 +38,17 @@ ApplicationConfiguration:
       DataDirectoryPath: "/chains/two"
   #    BoltDBOptions:
   #      FilePath: "./chains/privnet.bolt"
-  Addresses:
-    - ":20334" # in form of "[host]:[port][:announcedPort]"
+  P2P:
+    Addresses:
+      - ":20334" # in form of "[host]:[port][:announcedPort]"
+    DialTimeout: 3s
+    ProtoTickInterval: 2s
+    PingInterval: 30s
+    PingTimeout: 90s
+    MaxPeers: 10
+    AttemptConnPeers: 5
+    MinPeers: 3
   Relay: true
-  DialTimeout: 3
-  ProtoTickInterval: 2
-  PingInterval: 30
-  PingTimeout: 90
-  MaxPeers: 10
-  AttemptConnPeers: 5
-  MinPeers: 3
   Oracle:
     Enabled: false
     AllowedContentTypes:

commit 3ce1fc41a4e64d4eca602dd05a90ec2ddeb4a488

config: fix the default P2P ping settings
54c2aa858295a38f79b1bf375a79e48194d4ceb9 broke them, they're in seconds and we
have a 90s timeout.

pkg/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/nspcc-dev/neo-go/pkg/config/netmode"
 	"gopkg.in/yaml.v3"
@@ -66,8 +67,8 @@ func LoadFile(configPath string) (Config, error) {
 	config := Config{
 		ApplicationConfiguration: ApplicationConfiguration{
 			P2P: P2P{
-				PingInterval: 30,
-				PingTimeout:  30,
+				PingInterval: 30 * time.Second,
+				PingTimeout:  90 * time.Second,
 			},
 			RPC: RPC{
 				MaxIteratorResultItems: DefaultMaxIteratorResultItems,