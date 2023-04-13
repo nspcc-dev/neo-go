0c049f620fc5ec8f9b0757b44a4105bc73dc5df7
Anna Shaleva <shaleva.ann@nspcc.ru>
2023-04-13 13:02:36 +0300

    config: do not allow zero numbers for validators/committee history
    Signed-off-by: Anna Shaleva <shaleva.ann@nspcc.ru>

--- pkg/config/protocol_config_test.go
+++ pkg/config/protocol_config_test.go
@@ -111,6 +111,28 @@ func TestProtocolConfigurationValidation(t *testing.T) {
 	}
 	require.Error(t, p.Validate())
 	p = &ProtocolConfiguration{
+		StandbyCommittee: []string{
+			"02b3622bf4017bdfe317c58aed5f4c753f206b7db896046fa7d774bbc4bf7f8dc2",
+			"02103a7f7dd016558597f7960d27c516a4394fd968b9e65155eb4b013e4040406e",
+			"03d90c07df63e690ce77912e10ab51acc944b66860237b608c4f8f8309e71ee699",
+			"02a7bc55fe8684e0119768d104ba30795bdcc86619e864add26156723ed185cd62",
+		},
+		CommitteeHistory:  map[uint32]uint32{0: 0, 100: 4},
+		ValidatorsHistory: map[uint32]uint32{0: 1, 100: 4},
+	}
+	require.Error(t, p.Validate())
+	p = &ProtocolConfiguration{
+		StandbyCommittee: []string{
+			"02b3622bf4017bdfe317c58aed5f4c753f206b7db896046fa7d774bbc4bf7f8dc2",
+			"02103a7f7dd016558597f7960d27c516a4394fd968b9e65155eb4b013e4040406e",
+			"03d90c07df63e690ce77912e10ab51acc944b66860237b608c4f8f8309e71ee699",
+			"02a7bc55fe8684e0119768d104ba30795bdcc86619e864add26156723ed185cd62",
+		},
+		CommitteeHistory:  map[uint32]uint32{0: 1, 100: 4},
+		ValidatorsHistory: map[uint32]uint32{0: 0, 100: 4},
+	}
+	require.Error(t, p.Validate())
+	p = &ProtocolConfiguration{
 		Hardforks: map[string]uint32{
 			"Unknown": 123, // Unknown hard-fork.
 		},
