11bb733f1a2c56857891fd38f8b4b2da8c461220
Anna Shaleva <shaleva.ann@nspcc.ru>
2023-07-20 11:42:48 +0300

    interop: support ABORTMSG, ASSERT, ASSERTMSG opcodes
    Signed-off-by: Anna Shaleva <shaleva.ann@nspcc.ru>

--- pkg/interop/util/util.go
+++ pkg/interop/util/util.go
@@ -13,6 +13,25 @@ func Abort() {
 	neogointernal.Opcode0NoReturn("ABORT")
 }
 
+// AbortMsg terminates current execution with the specified message. Unlike
+// exception throwing with panic() it can't be recovered from.
+func AbortMsg(msg string) {
+	neogointernal.Opcode1NoReturn("ABORTMSG", msg)
+}
+
+// Assert terminates current execution if the condition specified is false. Unlike
+// exception throwing with panic() it can't be recovered from.
+func Assert(ok bool) {
+	neogointernal.Opcode1NoReturn("ASSERT", ok)
+}
+
+// AssertMsg terminates current execution with the specified message if the
+// condition specified is false. Unlike exception throwing with panic() it can't
+// be recovered from.
+func AssertMsg(ok bool, msg string) {
+	neogointernal.Opcode2NoReturn("ASSERTMSG", ok, msg)
+}
+
 // Equals compares a with b and will return true when a and b are equal. It's
 // implemented as an EQUAL VM opcode, so the rules of comparison are those
 // of EQUAL.
