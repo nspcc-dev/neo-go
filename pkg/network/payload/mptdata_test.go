3456d9222042c547c580eecfa470c17888fdd30d
Furetur <furetur@gmail.com>
2024-07-03 12:48:00 +0300

    mptdata: add test for MaxArraySize
    This commit adds a single test that covers the
    previously uncovered branch in the mptdata
    decoding algorithm.
    
    Signed-off-by: Furetur <furetur@gmail.com>

--- pkg/network/payload/mptdata_test.go
+++ pkg/network/payload/mptdata_test.go
@@ -21,4 +21,18 @@ func TestMPTData_EncodeDecodeBinary(t *testing.T) {
 		}
 		testserdes.EncodeDecodeBinary(t, d, new(MPTData))
 	})
+
+	t.Run("exceeds MaxArraySize", func(t *testing.T) {
+		bytes := []byte{
+			// The first byte represents the number 0x1.
+			// It encodes the size of the outer array (the number or rows in the Nodes matrix).
+			0x1,
+			// This sequence of 9 bytes represents the number 0xffffffffffffffff.
+			// It encodes the size of the first row in the Nodes matrix.
+			// This size exceeds the maximum array size, thus the decoder should
+			// return an error.
+			0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+		}
+		require.Error(t, testserdes.DecodeBinary(bytes, new(MPTData)))
+	})
 }
