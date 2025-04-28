c7929b26b12a8554a1288fff5467425d34b7f4b3
Anna Shaleva <shaleva.ann@nspcc.ru>
2025-04-28 16:38:45 +0300

    config: prohibit NeoFSStateSyncExtensions enabling
    This feature is not fully supported yet, #3793 is missing hence
    NeoFS-based state synchronisation from checkpoint won't work properly.
    
    Signed-off-by: Anna Shaleva <shaleva.ann@nspcc.ru>

--- pkg/config/protocol_config.go
+++ pkg/config/protocol_config.go
@@ -170,6 +170,9 @@ func (p *ProtocolConfiguration) Validate() error {
 			}
 		}
 	}
+	if p.NeoFSStateSyncExtensions {
+		return fmt.Errorf("NeoFSStateSyncExtensions implementation is not finished yet, please disable this option")
+	}
 	return nil
 }
 
