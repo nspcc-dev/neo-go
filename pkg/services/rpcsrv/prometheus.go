df80e5370a074c966174b9d2de88a45c754f04c0
Anna Shaleva <shaleva.ann@nspcc.ru>
2025-10-31 12:47:08 +0300

    rpcsrv: add RPC request counter
    Signed-off-by: Anna Shaleva <shaleva.ann@nspcc.ru>

--- pkg/services/rpcsrv/prometheus.go
+++ pkg/services/rpcsrv/prometheus.go
@@ -9,13 +9,18 @@ import (
 
 // Metrics used in monitoring service.
 var (
-	rpcTimes = map[string]prometheus.Histogram{}
+	rpcTimes    = map[string]prometheus.Histogram{}
+	rpcCounters = map[string]prometheus.Counter{}
 )
 
 func addReqTimeMetric(name string, t time.Duration) {
 	hist, ok := rpcTimes[name]
 	if ok {
 		hist.Observe(t.Seconds())
+	}
+	cnt, ok := rpcCounters[name]
+	if ok {
+		cnt.Inc()
 	}
 }
 
@@ -28,6 +33,14 @@ func regCounter(call string) {
 		},
 	)
 	prometheus.MustRegister(rpcTimes[call])
+	rpcCounters[call] = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Help:      "RPC " + call + " call count",
+			Name:      "rpc_" + strings.ToLower(call) + "_counter",
+			Namespace: "neogo",
+		},
+	)
+	prometheus.MustRegister(rpcCounters[call])
 }
 
 func init() {
