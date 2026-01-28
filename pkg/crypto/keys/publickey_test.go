fef4b3b02cc8fd73acdf37d9586f4bd5dbfef2b3
Roman Khimov <roman@nspcc.ru>
2026-01-28 23:47:03 +0300

    keys: make (*PublicKey).GetScriptHash zero-allocation method
    goos: linux
    goarch: amd64
    pkg: github.com/nspcc-dev/neo-go/pkg/crypto/keys
    cpu: AMD Ryzen 7 PRO 7840U w/ Radeon 780M Graphics
                           │ scripthash.old │           scripthash.new           │
                           │     sec/op     │   sec/op     vs base               │
    PublicGetScriptHash-16      340.7n ± 1%   324.8n ± 1%  -4.70% (p=0.000 n=10)
    
                           │ scripthash.old │           scripthash.new           │
                           │      B/op      │   B/op     vs base                 │
    PublicGetScriptHash-16       48.00 ± 0%   0.00 ± 0%  -100.00% (p=0.000 n=10)
    
                           │ scripthash.old │           scripthash.new            │
                           │   allocs/op    │ allocs/op   vs base                 │
    PublicGetScriptHash-16       1.000 ± 0%   0.000 ± 0%  -100.00% (p=0.000 n=10)
    
    Signed-off-by: Roman Khimov <roman@nspcc.ru>

--- pkg/crypto/keys/publickey_test.go
+++ pkg/crypto/keys/publickey_test.go
@@ -289,6 +289,13 @@ func BenchmarkPublicGetVerificationScript(t *testing.B) {
 	}
 }
 
+func BenchmarkPublicGetScriptHash(t *testing.B) {
+	k := getPubKey(t)
+	for t.Loop() {
+		_ = k.GetScriptHash()
+	}
+}
+
 func BenchmarkPublicDecodeBytes(t *testing.B) {
 	keyBytes, err := hex.DecodeString("03b209fd4f53a7170ea4444e0cb0a6bb6a53c2bd016926989cf85f9b0fba17a70c")
 	require.NoError(t, err)
