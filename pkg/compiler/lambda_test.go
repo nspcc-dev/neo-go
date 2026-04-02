343403d6bc1ac8902f49adfe3d422c9cdf6e1284
Tural Devrishev <tural@nspcc.ru>
2026-04-02 18:33:58 +0300

    compiler: include lambdas into DebugInfo
    Close #3619.
    
    Signed-off-by: Tural Devrishev <tural@nspcc.ru>

--- pkg/compiler/lambda_test.go
+++ pkg/compiler/lambda_test.go
@@ -2,7 +2,11 @@ package compiler_test
 
 import (
 	"math/big"
+	"strings"
 	"testing"
+
+	"github.com/nspcc-dev/neo-go/pkg/compiler"
+	"github.com/stretchr/testify/require"
 )
 
 func TestFuncLiteral(t *testing.T) {
@@ -25,4 +29,49 @@ func TestCallInPlace(t *testing.T) {
 		return a
 	}`
 	eval(t, src, big.NewInt(111))
+}
+
+func TestLambdaInDebugInfo(t *testing.T) {
+	srcSimple := `package main
+		import "github.com/nspcc-dev/neo-go/pkg/interop/runtime"
+		func PublicContractMethod() {
+			var f = func() {
+				runtime.Log("bla")
+			}
+		
+			f()
+			f()
+		}
+	`
+	srcFromArray := `package main
+		import "github.com/nspcc-dev/neo-go/pkg/interop/runtime"
+		func PublicContractMethod() {
+			arr := make([]func(), 1)
+			arr[0] = func() { runtime.Log("bla") }
+			f := arr[0]
+			f()
+		}
+	`
+	srcFromAnotherFunc := `package main
+		import "github.com/nspcc-dev/neo-go/pkg/interop/runtime"
+		func PublicContractMethod() {
+			var f = func() func() {
+				return func() { runtime.Log("bla") }
+			}()
+			f()
+		}
+	`
+	for _, src := range []string{srcSimple, srcFromArray, srcFromAnotherFunc} {
+		_, di, err := compiler.CompileWithOptions("test.go", strings.NewReader(src), nil)
+		require.NoError(t, err)
+		require.GreaterOrEqual(t, len(di.Methods), 2)
+		require.True(t, func() bool {
+			for _, methodDebugInfo := range di.Methods {
+				if strings.Contains(methodDebugInfo.Name.Name, "lambda") {
+					return true
+				}
+			}
+			return false
+		}())
+	}
 }
