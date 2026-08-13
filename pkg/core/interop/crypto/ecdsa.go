ccb9268fd69b8f7a1798d989ff386c742feece5a
Tural Devrishev <tural@nspcc.ru>
2026-08-13 12:56:05 +0300

    interop: add benchmarks for interop prices
    Signed-off-by: Tural Devrishev <tural@nspcc.ru>

--- pkg/core/interop/crypto/ecdsa.go
+++ pkg/core/interop/crypto/ecdsa.go
@@ -45,6 +45,51 @@ func ECDSASecp256r1CheckMultisig(ic *interop.Context) error {
 	return nil
 }
 
+func ECDSASecp256r1CheckMultisigDummy(ic *interop.Context) error {
+	pkeys, err := ic.VM.Estack().PopSigElements()
+	if err != nil {
+		return fmt.Errorf("wrong key parameters: %w", err)
+	}
+	sigs, err := ic.VM.Estack().PopSigElements()
+	if err != nil {
+		return fmt.Errorf("wrong signature parameters: %w", err)
+	}
+	if err := ic.VM.AddPicoGas(ic.BaseExecFee() * fee.ECDSAVerifyPrice * int64(len(pkeys))); err != nil {
+		return err
+	}
+	// It's ok to have more keys than there are signatures (it would
+	// just mean that some keys didn't sign), but not the other way around.
+	if len(pkeys) < len(sigs) {
+		return errors.New("more signatures than there are keys")
+	}
+	if ic.IsHardforkEnabled(config.HFGorgon) {
+		for i, s := range sigs {
+			if len(s) != keys.SignatureLen {
+				return fmt.Errorf("wrong signature #%d length: expected %d, got %d", i, keys.SignatureLen, len(s))
+			}
+		}
+	}
+	message := hash.NetSha256(ic.Network, ic.Container).BytesBE()
+	m, n := len(sigs), len(pkeys)
+	sigok := true
+	for i, j := 0, 0; i < m && j < n; {
+		pkey, err := keys.NewPublicKeyFromBytes(pkeys[j], elliptic.P256())
+		if err != nil {
+			return fmt.Errorf("invalid key #%d: %w", j, err)
+		}
+		if pkey.Verify(sigs[i], message) {
+			i++
+		}
+		j++
+		if m-i > n-j {
+			sigok = false
+			break
+		}
+	}
+	ic.VM.Estack().PushItem(stackitem.Bool(sigok))
+	return nil
+}
+
 // ECDSASecp256r1CheckSig checks ECDSA signature using Secp256r1 elliptic curve.
 func ECDSASecp256r1CheckSig(ic *interop.Context) error {
 	keyb := ic.VM.Estack().Pop().Bytes()
